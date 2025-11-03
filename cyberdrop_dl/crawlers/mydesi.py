2c1ede20d5cadfb5002c6ccabcb550ea3bb63a35
Jacob <admin@script-ware.net>
2025-11-03 16:42:20 -0500

    Add MyDesi.net Support
    Add support for MyDesi.net

---
+++ cyberdrop_dl/crawlers/mydesi.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import json
+from typing import TYPE_CHECKING, ClassVar
+
+from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths
+from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
+from cyberdrop_dl.utils.utilities import error_handling_wrapper
+
+if TYPE_CHECKING:
+    from cyberdrop_dl.data_structures.url_objects import ScrapeItem
+
+
+PRIMARY_URL = AbsoluteHttpURL("https://mydesi.net")
+
+
+class MyDesiCrawler(Crawler):
+    SUPPORTED_DOMAINS = "fry99.com", "lolpol.com", "mydesi.net"
+    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Videos": "/title", "Search": "/search/"}
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
+    DOMAIN: ClassVar[str] = "mydesi.net"
+    FOLDER_DOMAIN: ClassVar[str] = "MyDesi"
+
+    async def fetch(self, scrape_item: ScrapeItem) -> None:
+        if scrape_item.url.parts[1] == "search":
+            return await self.search(scrape_item)
+        elif len(scrape_item.url.parts) >= 2 and len(scrape_item.url.parts) <= 3:
+            return await self.video(scrape_item)
+        raise ValueError
+
+    @error_handling_wrapper
+    async def video(self, scrape_item: ScrapeItem) -> None:
+        if await self.check_complete_from_referer(scrape_item):
+            return
+        soup = await self.request_soup(scrape_item.url)
+        download_url = await self.select_highest_quality_video(soup)
+        title = soup.select_one("div.col-12.col-md-8.col-left > h1").text.strip()
+        filename, ext = self.get_filename_and_ext(download_url.name)
+
+        metadata_script = json.loads(soup.select_one("script[type='application/ld+json']").string)
+        upload_date = metadata_script.get("subjectOf", {}).get("uploadDate")
+        scrape_item.possible_datetime = self.parse_date(upload_date) if upload_date else None
+
+        return await self.handle_file(download_url, scrape_item, filename, ext, custom_filename=f"{title}{ext}")
+
+    @error_handling_wrapper
+    async def search(self, scrape_item: ScrapeItem) -> None:
+        album_initialized: bool = False
+        async for soup in self.web_pager(scrape_item.url, next_page_selector="li.page-item > a:contains('»')"):
+            if not album_initialized:
+                title = self.create_title(soup.select_one("title").text.split("“")[1].split("”")[0])
+                scrape_item.setup_as_album(title)
+                album_initialized = True
+
+            videos = soup.select("a.infos")
+            for video in videos:
+                video_url = self.parse_url(video.get("href"))
+                new_scrape_item = scrape_item.create_child(video_url)
+                self.create_task(self.run(new_scrape_item))
+
+    async def select_highest_quality_video(self, soup) -> AbsoluteHttpURL:
+        video_links = soup.select("a.btn.btn-dark.btn-sm")
+        quality_map = {i.get("title"): i.get("href") for i in video_links}
+        if not quality_map:
+            raise ValueError("No video links found")
+        if "Original" in quality_map:
+            selected_url = quality_map["Original"]
+        else:
+            selected_url = quality_map[max(quality_map.keys(), key=lambda x: int(x.rstrip("p")))]
+        return self.parse_url(selected_url)
