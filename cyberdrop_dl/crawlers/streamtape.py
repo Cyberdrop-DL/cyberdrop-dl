a55988f88b68ef7643c4324dab5db6cdf3c02b42
Jacob <admin@script-ware.net>
2025-11-03 13:12:43 -0500

    Add player support
    Add player support and improve canonical URLs

--- cyberdrop_dl/crawlers/streamtape.py
+++ cyberdrop_dl/crawlers/streamtape.py
@@ -31,23 +31,23 @@ _REGEX_SELECTORS = RegexSelectors()
 
 
 class StreamtapeCrawler(Crawler):
-    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Videos": "/v/"}
+    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Videos": "/v/", "Player": "/e/"}
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
     DOMAIN: ClassVar[str] = "streamtape.com"
     FOLDER_DOMAIN: ClassVar[str] = "Streamtape"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
-        if scrape_item.url.parts[1] == "v":
+        if scrape_item.url.parts[1] in ("e", "v"):
             return await self.video(scrape_item)
         else:
             raise ValueError
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem) -> None:
+        scrape_item.url = await self.create_canonical_url(scrape_item)
         if await self.check_complete_from_referer(scrape_item):
             return
         soup = await self.request_soup(scrape_item.url)
-        filename, ext = self.get_filename_and_ext(scrape_item.url.name)
         scripts = soup.find_all("script")[::-1]
         for script in scripts:
             if "document.getElementById('ideoooolink').innerHTML" in script.text:
@@ -56,6 +56,7 @@ class StreamtapeCrawler(Crawler):
             raise RuntimeError("Could not find video script")
         generator_url = await self.decode_links(script.text)
         download_url = await self._get_redirect_url(generator_url)
+        filename, ext = self.get_filename_and_ext(download_url.name)
 
         return await self.handle_file(download_url, scrape_item, filename, ext)
 
@@ -74,3 +75,6 @@ class StreamtapeCrawler(Crawler):
             if decoded_link.startswith("streamtape.com/get_video?id="):
                 decoded_links.append(self.parse_url(f"https://{decoded_link}"))
         return max(set(decoded_links), key=decoded_links.count)
+
+    async def create_canonical_url(self, scrape_item: ScrapeItem) -> AbsoluteHttpURL:
+        return self.parse_url(f"https://{scrape_item.url.host}/v/{scrape_item.url.parts[2]}")
--- tests/crawlers/test_cases/streamtape.py
+++ tests/crawlers/test_cases/streamtape.py
@@ -7,7 +7,19 @@ TEST_CASES = [
             {
                 "url": r"re:https://\d+\.tapecontent.net/radosgw/oelrLvaa3lIJyrR/.*?/TnkrBh\.mp4",
                 "filename": "TnkrBh.mp4",
-                "referer": "https://streamtape.com/v/oelrLvaa3lIJyrR/TnkrBh.mp4",
+                "referer": "https://streamtape.com/v/oelrLvaa3lIJyrR",
+                "datetime": None,
+            }
+        ],
+    ),
+    # Video Player
+    (
+        "https://streamtape.com/e/oelrLvaa3lIJyrR",
+        [
+            {
+                "url": r"re:https://\d+\.tapecontent.net/radosgw/oelrLvaa3lIJyrR/.*?/TnkrBh\.mp4",
+                "filename": "TnkrBh.mp4",
+                "referer": "https://streamtape.com/v/oelrLvaa3lIJyrR",
                 "datetime": None,
             }
         ],
