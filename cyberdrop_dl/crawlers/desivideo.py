c387d03e53899e69a9bfc836fccb622f92110134
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-11-07 08:38:10 +0000

    Apply suggestions from code review

--- cyberdrop_dl/crawlers/desivideo.py
+++ cyberdrop_dl/crawlers/desivideo.py
@@ -16,20 +16,22 @@ PRIMARY_URL = AbsoluteHttpURL("https://desivideo.net")
 
 
 class DesiVideoCrawler(Crawler):
-    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Videos": "/videos/", "Search": "/search?s="}
+    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Video": "/videos/<video_id>/...", "Search": "/search?s=<query>",}
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
     DOMAIN: ClassVar[str] = "desivideo.net"
     FOLDER_DOMAIN: ClassVar[str] = "DesiVideo"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
-        if scrape_item.url.parts[1] == "search" and scrape_item.url.query.get("s"):
-            return await self.search(scrape_item)
-        elif scrape_item.url.parts[1] == "videos":
-            return await self.video(scrape_item)
-        raise ValueError
+        match scrape_item.url.parts[1:]:
+            case ["search"] if query:= scrape_item.url.query.get("s"):
+               return await self.search(scrape_item, query)
+            case ["videos", video_id, *_]
+                return await self.video(scrape_item, video_id)
+            case _:
+                raise ValueError
 
     @error_handling_wrapper
-    async def video(self, scrape_item: ScrapeItem) -> None:
+    async def video(self, scrape_item: ScrapeItem, video_id: str) -> None:
         if await self.check_complete_from_referer(scrape_item):
             return
         soup = await self.request_soup(scrape_item.url)
@@ -37,15 +39,13 @@ class DesiVideoCrawler(Crawler):
         title = soup.select_one("strong.bread-current").text.strip()
         filename, ext = self.get_filename_and_ext(video_url.name)
 
+        custom_filename = self.create_custom_filename(title, ext, file_id=video_id)
         return await self.handle_file(video_url, scrape_item, filename, ext, custom_filename=f"{title}{ext}")
 
     @error_handling_wrapper
-    async def search(self, scrape_item: ScrapeItem) -> None:
-        title = self.create_title(scrape_item.url.query.get("s"))
+    async def search(self, scrape_item: ScrapeItem, query: str) -> None:
+        title = self.create_title(query)
         scrape_item.setup_as_album(title)
-        async for soup in self.web_pager(scrape_item.url, next_page_selector="li.page-item > a:contains('›')"):  # noqa: RUF001
-            videos: tuple[BeautifulSoup] = soup.select("div.videos-list > article > a")
-            for video in videos:
-                video_url = self.parse_url(video.get("href"))
-                new_scrape_item = scrape_item.create_child(video_url)
+        async for soup in self.web_pager(scrape_item.url, next_page_selector="li.page-item > a:contains('›')"): 
+            for _, new_scrape_item in self.iter_children(scrape_item, soup,"div.videos-list > article > a"):
                 self.create_task(self.run(new_scrape_item))
