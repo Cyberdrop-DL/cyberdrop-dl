efd7908c2aadb41f6f012be9189c01abe6786c7b
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-11-08 05:18:03 -0500

    refactor: parse resolution

--- cyberdrop_dl/crawlers/xgroovy.py
+++ cyberdrop_dl/crawlers/xgroovy.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, ClassVar, NamedTuple
 
 from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths
+from cyberdrop_dl.data_structures.mediaprops import Resolution
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 from cyberdrop_dl.utils import css, open_graph
 from cyberdrop_dl.utils.utilities import error_handling_wrapper
@@ -13,22 +14,16 @@ if TYPE_CHECKING:
     from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
 
-PRIMARY_URL = AbsoluteHttpURL("https://xgroovy.com")
-
-
-class Selectors:
-    VIDEO = "video#main_video"
-    GIF = "div.gif-video-wrapper > video"
-    PORNSTAR_NAME = "h2.object-title"
+class Selector:
+    GIF = css.CssAttributeSelector("div.gif-video-wrapper > video", "src")
+    VIDEO_SRC = "video#main_video source"
+    COLLECTION_TITLE = "h2.object-title"
     SEARCH_VIDEOS = "div.list-videos a.popito"
     NEXT_PAGE = "div.pagination-holder li.next > a"
 
 
-_SELECTORS = Selectors()
-
-
 class Format(NamedTuple):
-    resolution: str
+    resolution: Resolution
     link_str: str
 
 
@@ -61,8 +56,8 @@ class XGroovyCrawler(Crawler):
     }
     DOMAIN: ClassVar[str] = "xgroovy"
     FOLDER_DOMAIN: ClassVar[str] = "XGroovy"
-    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
-    NEXT_PAGE_SELECTOR: ClassVar[str] = _SELECTORS.NEXT_PAGE
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://xgroovy.com")
+    NEXT_PAGE_SELECTOR: ClassVar[str] = Selector.NEXT_PAGE
     _RATE_LIMIT = 3, 10
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
@@ -84,8 +79,8 @@ class XGroovyCrawler(Crawler):
             return
 
         soup = await self.request_soup(scrape_item.url)
-        link = self.parse_url(css.get_attr(css.select_one(soup, _SELECTORS.GIF), "src"))
-        return await self.download_video(scrape_item, gif_id, soup, link)
+        link = self.parse_url(Selector.GIF(soup))
+        return await self._video(scrape_item, gif_id, soup, link)
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem, video_id: str) -> None:
@@ -93,21 +88,20 @@ class XGroovyCrawler(Crawler):
             return
 
         soup = await self.request_soup(scrape_item.url)
-        best_format: Format = _get_best_format(css.select_one(soup, _SELECTORS.VIDEO))
-        return await self.download_video(
-            scrape_item, video_id, soup, self.parse_url(best_format.link_str), resolution=best_format.resolution
-        )
+        best_format = _get_best_format(soup)
+        link = self.parse_url(best_format.link_str)
+        return await self._video(scrape_item, video_id, soup, link, resolution=best_format.resolution)
 
-    async def download_video(
+    async def _video(
         self,
         scrape_item: ScrapeItem,
         file_id: str,
         soup: BeautifulSoup,
         link: AbsoluteHttpURL,
-        resolution: str | None = None,
+        resolution: Resolution | None = None,
     ):
         filename, ext = self.get_filename_and_ext(link.name)
-        title = open_graph.get_title(soup)
+        title = open_graph.title(soup)
         scrape_item.possible_datetime = self.parse_iso_date(css.get_json_ld_date(soup))
         custom_filename = self.create_custom_filename(title, ext, file_id=file_id, resolution=resolution)
         return await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename)
@@ -117,20 +111,22 @@ class XGroovyCrawler(Crawler):
         title: str = ""
         async for soup in self.web_pager(scrape_item.url):
             if not title:
-                name = name or css.select_one_get_text(soup, _SELECTORS.PORNSTAR_NAME)
+                name = name or css.select_one_get_text(soup, Selector.COLLECTION_TITLE)
                 title = self.create_title(f"{name} [{collection_type}]")
                 scrape_item.setup_as_album(title)
 
-            for _, new_scrape_item in self.iter_children(scrape_item, soup, _SELECTORS.SEARCH_VIDEOS):
+            for _, new_scrape_item in self.iter_children(scrape_item, soup, Selector.SEARCH_VIDEOS):
                 self.create_task(self.run(new_scrape_item))
 
 
-def _get_best_format(video_tag: Tag) -> Format:
+def _get_best_format(soup: Tag) -> Format:
     def parse():
-        for src in video_tag.select("source"):
+        for src in soup.select(Selector.VIDEO_SRC):
             url = css.get_attr(src, "src")
-            title = css.get_attr_or_none(src, "title") or "0p"
-            resolution = int(title.replace("p", ""))
+            if title := css.get_attr_or_none(src, "title"):
+                resolution = Resolution.parse(title)
+            else:
+                resolution = Resolution.unknown()
             yield Format(resolution, url)
 
-    return max(parse(), key=lambda f: int(f.resolution))
+    return max(parse())
