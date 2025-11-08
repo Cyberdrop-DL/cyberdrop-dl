ced17b20692dc64b5699de6c4e33e357dcf7d6d6
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-11-08 00:27:30 -0500

    perf: remove additional request for title

--- cyberdrop_dl/crawlers/tnaflix.py
+++ cyberdrop_dl/crawlers/tnaflix.py
@@ -69,13 +69,13 @@ class TNAFlixCrawler(Crawler):
 
     @error_handling_wrapper
     async def collection(self, scrape_item: ScrapeItem, collection_type: str, name: str | None = None) -> None:
-        if not name:
-            soup = await self.request_soup(scrape_item.url)
-            name = css.select_one_get_text(soup, Selector.COLLECTION_TITLE)
-
-        title = self.create_title(f"{name} - [{collection_type}]")
-        scrape_item.setup_as_album(title)
+        title: str = ""
         async for soup in self.web_pager(scrape_item.url):
+            if not title:
+                name = name or css.select_one_get_text(soup, Selector.COLLECTION_TITLE)
+                title = self.create_title(f"{name} - [{collection_type}]")
+                scrape_item.setup_as_album(title)
+
             for _, new_scrape_item in self.iter_children(scrape_item, soup, Selector.VIDEOS_THUMBS):
                 self.create_task(self.run(new_scrape_item))
 
