1305a8d9db2dc5669c94565ff80508aecf19364b
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-11-14 19:14:56 -0500

    fix: ignore fuse filesystem that report 0B as free space

--- cyberdrop_dl/managers/storage_manager.py
+++ cyberdrop_dl/managers/storage_manager.py
@@ -16,6 +16,8 @@ from cyberdrop_dl.exceptions import InsufficientFreeSpaceError
 from cyberdrop_dl.utils.logger import log, log_debug
 
 if TYPE_CHECKING:
+    from collections.abc import Generator
+
     from psutil._common import sdiskpart
 
     from cyberdrop_dl.data_structures.url_objects import MediaItem
@@ -58,15 +60,8 @@ class StorageManager:
         self._period: int = 2  # how often the check_free_space_loop will run (in seconds)
         self._log_period: int = 10  # log storage details every <x> loops, AKA log every 20 (2x10) seconds,
         self._timedelta_period = timedelta(seconds=self._period)
-        self._partitions = []
-        for p in psutil.disk_partitions(all=True):
-            try:
-                part = DiskPartition.from_psutil(p)
-            except OSError as e:
-                msg = f"Unable to get information about {p.mountpoint}. All files with that mountpoint as target will be skipped: {e!r}"
-                log(msg, 40)
-            else:
-                self._partitions.append(part)
+
+        self._partitions = list(_get_disk_partitions())
 
         self._loop = asyncio.create_task(self._check_free_space_loop())
         self._unavailable_mounts: set[Path] = set()
@@ -117,43 +112,43 @@ class StorageManager:
         except asyncio.CancelledError:
             pass
 
+    async def _check_nt_network_drive(self, folder: Path) -> None:
+        """Checks is the drive of this folder is a Windows network drive (UNC or unknown mapped drive) and exists."""
+        # See: https://github.com/jbsparrow/CyberDropDownloader/issues/860
+        if not psutil.WINDOWS:
+            return
+
+        # We can discard mapped drives because they would have been converted to UNC path at startup
+        # calling resolve on a mapped network drive returns its UNC path
+        # it would only still be a mapped drive is the network address is not available
+        is_mapped_drive = ":" in folder.drive and len(folder.drive) == 2
+        is_unc_path = folder.drive.startswith("\\\\")
+        if is_mapped_drive or not is_unc_path:
+            return
+
+        folder_drive = drive_as_path(folder.drive)
+        async with self._mount_addition_locks[folder_drive]:
+            if folder_drive in itertools.chain(self._unavailable_mounts, self.mounts):
+                return
+            msg = f"Checking new possible network_drive: '{folder_drive}' for folder '{folder}'"
+            log_debug(msg)
+
+            try:
+                is_dir = await asyncio.to_thread(folder_drive.is_dir)
+            except OSError:
+                is_dir = False
+            if is_dir:
+                net_drive = DiskPartition(folder_drive, folder_drive, "network_drive", "")
+                self._partitions.append(net_drive)
+            else:
+                self._unavailable_mounts.add(folder_drive)
+
     async def _has_sufficient_space(self, folder: Path) -> bool:
         """Checks if there is enough free space to download to this folder.
 
         `folder` must be an absolute path"""
 
-        async def check_nt_network_drive():
-            """Checks is the drive of this folder is a Windows network drive (UNC or unknown mapped drive) and exists."""
-            # See: https://github.com/jbsparrow/CyberDropDownloader/issues/860
-            if not psutil.WINDOWS:
-                return
-
-            # We can discard mapped drives because they would have been converted to UNC path at startup
-            # calling resolve on a mapped network drive returns its UNC path
-            # it would only still be a mapped drive is the network address is not available
-            is_mapped_drive = ":" in folder.drive and len(folder.drive) == 2
-            is_unc_path = folder.drive.startswith("\\\\")
-            if is_mapped_drive or not is_unc_path:
-                return
-
-            folder_drive = drive_as_path(folder.drive)
-            async with self._mount_addition_locks[folder_drive]:
-                if folder_drive in itertools.chain(self._unavailable_mounts, self.mounts):
-                    return
-                msg = f"Checking new possible network_drive: '{folder_drive}' for folder '{folder}'"
-                log_debug(msg)
-
-                try:
-                    is_dir = await asyncio.to_thread(folder_drive.is_dir)
-                except OSError:
-                    is_dir = False
-                if is_dir:
-                    net_drive = DiskPartition(folder_drive, folder_drive, "network_drive", "")
-                    self._partitions.append(net_drive)
-                else:
-                    self._unavailable_mounts.add(folder_drive)
-
-        await check_nt_network_drive()
+        await self._check_nt_network_drive(folder)
         mount = get_mount_point(folder, self.mounts)
         if not mount:
             return False
@@ -161,24 +156,47 @@ class StorageManager:
         async with self._mount_addition_locks[mount]:
             if mount not in self._free_space:
                 # Manually query this mount now. Next time it will be part of the loop
-                try:
-                    result = await asyncio.to_thread(psutil.disk_usage, str(mount))
-                    free_space = result.free
-                except OSError as e:
-                    if "operation not supported" in str(e).casefold():
-                        msg = f"Unable to get free space from mount point ('{mount!s}')'. Skipping free space check"
-                        log(msg, 40, exc_info=e)
 
-                        free_space = 0
-                    else:
-                        raise
-
-                self._free_space[mount] = free_space
+                self._free_space[mount] = await self._get_free_space(mount)
                 self._used_mounts.add(mount)
                 log(f"A new mountpoint ('{mount!s}') will be used for '{folder}'")
                 log(self._simplified_stats)
 
+        free_space = self._free_space[mount]
+        if free_space == -1:
+            return True
         return self._free_space[mount] > self.manager.config_manager.global_settings_data.general.required_free_space
+
+    async def _get_free_space(self, mount: Path) -> int:
+        exc_info = None
+        free_space = 0
+
+        try:
+            result = await asyncio.to_thread(psutil.disk_usage, str(mount))
+            free_space = result.free
+        except OSError as e:
+            if "operation not supported" in str(e).casefold():
+                exc_info = e
+            else:
+                raise
+
+        if exc_info or (free_space == 0 and self._is_fuse_fs(mount)):
+            msg = f"Unable to get free space from mount point ('{mount!s}')'. Skipping free space check"
+            log(msg, 40, exc_info=exc_info)
+            free_space = -1
+
+        return free_space
+
+    def _is_fuse_fs(self, mount: Path) -> bool:
+        for p in self._partitions:
+            try:
+                mount.relative_to(p.mountpoint)
+            except ValueError:
+                continue
+            else:
+                if "fuse" in p.fstype:
+                    return True
+        return False
 
     async def _check_free_space_loop(self) -> None:
         """Infinite loop to get free space of all used mounts and update internal dict"""
@@ -189,13 +207,14 @@ class StorageManager:
             self._updated.clear()
             last_check += 1
             if self._used_mounts:
-                used_mounts = sorted(self._used_mounts)
-                tasks = [asyncio.to_thread(psutil.disk_usage, str(mount)) for mount in used_mounts]
+                used_mounts = sorted(mount for mount in self._used_mounts if self._free_space[mount] != -1)
+                tasks = [self._get_free_space(mount) for mount in used_mounts]
                 results = await asyncio.gather(*tasks)
-                for mount, result in zip(used_mounts, results, strict=True):
-                    self._free_space[mount] = result.free
+                for mount, free_space in zip(used_mounts, results, strict=True):
+                    self._free_space[mount] = free_space
                 if last_check % self._log_period == 0:
                     log_debug(self._simplified_stats)
+
             self._updated.set()
             await asyncio.sleep(self._period)
 
@@ -225,3 +244,12 @@ def get_mount_point(folder: Path, all_mounts: tuple[Path, ...]) -> Path | None:
 def drive_as_path(drive: str) -> Path:
     is_mapped_drive = ":" in drive and len(drive) == 2
     return Path(f"{drive}/" if is_mapped_drive else drive)
+
+
+def _get_disk_partitions() -> Generator[DiskPartition]:
+    for p in psutil.disk_partitions(all=True):
+        try:
+            yield DiskPartition.from_psutil(p)
+        except OSError as e:
+            msg = f"Unable to get information about {p.mountpoint}. All files with that mountpoint as target will be skipped: {e!r}"
+            log(msg, 40)
