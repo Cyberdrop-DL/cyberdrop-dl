c68c4a3daa1febce263bb42a1cb19190a74ea654
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-11-18 20:15:08 -0500

    refactor: simplify

--- cyberdrop_dl/crawlers/mydesi.py
+++ cyberdrop_dl/crawlers/mydesi.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, ClassVar
 
 from cyberdrop_dl.crawlers.crawler import Crawler, SupportedDomains, SupportedPaths
-from cyberdrop_dl.data_structures import Resolution
+from cyberdrop_dl.data_structures.mediaprops import Resolution
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 from cyberdrop_dl.utils import css
 from cyberdrop_dl.utils.utilities import error_handling_wrapper
 
 if TYPE_CHECKING:
+    from collections.abc import Generator
+
     from bs4 import BeautifulSoup
 
     from cyberdrop_dl.data_structures.url_objects import ScrapeItem
@@ -40,14 +42,14 @@ class MyDesiCrawler(Crawler):
             return
 
         soup = await self.request_soup(scrape_item.url)
-        resolution, download_url = self.select_highest_quality_video(soup)
-        filename, ext = self.get_filename_and_ext(download_url.name)
+        resolution, link = max(_parse_formats(soup))
+        link = self.parse_url(link)
+        _, ext = self.get_filename_and_ext(link.name)
         metadata: dict[str, str] = css.get_json_ld(soup)["subjectOf"]
-        upload_date = metadata.get("uploadDate", "")
         title = metadata["name"]
-        scrape_item.possible_datetime = self.parse_iso_date(upload_date)
+        scrape_item.possible_datetime = self.parse_iso_date(metadata.get("uploadDate", ""))
         custom_filename = self.create_custom_filename(title, ext, resolution=resolution)
-        return await self.handle_file(download_url, scrape_item, filename, ext, custom_filename=custom_filename)
+        return await self.handle_file(link, scrape_item, title, ext, custom_filename=custom_filename)
 
     @error_handling_wrapper
     async def search(self, scrape_item: ScrapeItem, query: str) -> None:
@@ -58,12 +60,10 @@ class MyDesiCrawler(Crawler):
             for _, new_scrape_item in self.iter_children(scrape_item, soup, "a.infos"):
                 self.create_task(self.run(new_scrape_item))
 
-    def select_highest_quality_video(self, soup: BeautifulSoup) -> tuple[Resolution, AbsoluteHttpURL]:
-        def parse():
-            for src in soup.select("#video-rate > a"):
-                quality = css.get_attr(src, "title")
-                link = css.get_attr(src, "href")
-                resolution = Resolution.highest() if "original" in quality.casefold() else Resolution.parse(quality)
-                yield resolution, self.parse_url(link)
 
-        return max(parse())
+def _parse_formats(soup: BeautifulSoup) -> Generator[tuple[Resolution, str]]:
+    for src in soup.select("#video-rate > a"):
+        quality = css.get_attr(src, "title")
+        link = css.get_attr(src, "href")
+        resolution = Resolution.highest() if "original" in quality.casefold() else Resolution.parse(quality)
+        yield resolution, link
