f9884359d6a2c5c36af469824e418d69feeed040
Barbarella <barbarella6666666@protonmail.com>
2025-11-24 18:19:40 -0300

    test: adding test case

---
+++ tests/crawlers/test_cases/thothub.py
@@ -0,0 +1,14 @@
+DOMAIN = "thothub"
+TEST_CASES = [
+    (
+        "https://thothub.to/videos/1415181/kateharperxo/",
+        [
+            {
+                "url": "https://thothub.to/videos/1415181/kateharperxo/",
+                "filename": "Kateharperxo [1415181].mp4",
+                "referer": "https://thothub.to/videos/1415181/kateharperxo/",
+                "album_id": None,
+            }
+        ],
+    ),
+]
