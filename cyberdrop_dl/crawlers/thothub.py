5f20b312c4264508a72131aa6b0857cca975f598
Barbarella <barbarella6666666@protonmail.com>
2025-11-26 08:16:46 -0300

    refactor: fix code format

--- cyberdrop_dl/crawlers/thothub.py
+++ cyberdrop_dl/crawlers/thothub.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-from typing import ClassVar
+from typing import TYPE_CHECKING, ClassVar
 
 from cyberdrop_dl.crawlers._kvs import KernelVideoSharingCrawler
-from cyberdrop_dl.crawlers.crawler import SupportedPaths
-from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL, ScrapeItem
+from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
+
+if TYPE_CHECKING:
+    from cyberdrop_dl.crawlers.crawler import SupportedPaths
+    from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
 
 class ThotHubCrawler(KernelVideoSharingCrawler):
