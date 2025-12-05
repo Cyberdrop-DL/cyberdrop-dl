04ff18acfc93a336edc91f77c6eec9d5bd0fd241
Barbarella <barbarella6666666@protonmail.com>
2025-12-05 11:03:20 -0300

    fix: search

--- cyberdrop_dl/crawlers/pmvhaven.py
+++ cyberdrop_dl/crawlers/pmvhaven.py
@@ -14,6 +14,8 @@ from cyberdrop_dl.utils.utilities import error_handling_wrapper
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator
 
+    from bs4 import BeautifulSoup
+
     from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
 class Selectors:
@@ -68,67 +70,42 @@ class PMVHavenCrawler(Crawler):
         title = self.create_title(title)
         scrape_item.setup_as_profile(title)
 
-        info_table = json.loads(css.select_text(soup, Selectors.APP_JSON))
-        video_info_list = [data for data in info_table if isinstance(data, dict) and "videoUrl" in data]
-        for video_info in video_info_list:
-            await self.process_video_info(scrape_item, info_table, video_info)
+        await self.process_video_list(scrape_item, soup)
 
 
     @error_handling_wrapper
     async def playlist(self, scrape_item: ScrapeItem, add_suffix: bool = True) -> None:
-        playlist_id = scrape_item.url.name
-        add_data = {"mode": "loadPlaylistVideos", "playlist": playlist_id}
-        api_url = API_ENTRYPOINT / "playlists"
-        title: str = ""
-        async for json_resp in self.api_pager(api_url, add_data, check_key="videos"):
-            if not title:
-                playlist_name: str = json_resp["playlist"]["name"]
-                title = f"{playlist_name} [playlist]" if add_suffix else playlist_name
-                title = self.create_title(title, playlist_id)
-                scrape_item.setup_as_album(title, album_id=playlist_id)
-            await self.iter_video_info(scrape_item, json_resp["videos"])
+        ...
 
     @error_handling_wrapper
     async def model(self, scrape_item: ScrapeItem) -> None:
-        model_name = scrape_item.url.name
-        add_data = {"mode": "SearchStar", "star": model_name, "profile": None}
-        await self._generic_search_pager(scrape_item, add_data, model_name, "model")
+        ...
 
     @error_handling_wrapper
     async def creator(self, scrape_item: ScrapeItem) -> None:
-        creator_name = scrape_item.url.name
-        add_data = {"mode": "SearchCreator", "creator": creator_name, "profile": None}
-        await self._generic_search_pager(scrape_item, add_data, creator_name, "creator")
+        ...
 
     @error_handling_wrapper
     async def music(self, scrape_item: ScrapeItem) -> None:
-        song_name = scrape_item.url.name
-        add_data = {"mode": "SearchMusic", "music": song_name, "profile": None}
-        await self._generic_search_pager(scrape_item, add_data, song_name, "music")
+        ...
 
     @error_handling_wrapper
     async def search(self, scrape_item: ScrapeItem) -> None:
-        search_value = scrape_item.url.name
-        add_data = {"mode": "DefaultSearch", "data": search_value, "profile": None}
-        await self._generic_search_pager(scrape_item, add_data, search_value, "search")
+        soup = await self.request_soup(scrape_item.url)
+        tags = scrape_item.url.query.get("tags")
+        title = f"{tags} [search]"
+        title = self.create_title(title)
+        scrape_item.setup_as_album(title)
+
+        await self.process_video_list(scrape_item, soup)
 
     @error_handling_wrapper
     async def category(self, scrape_item: ScrapeItem) -> None:
-        category_name = ""
-        for cat in CATEGORIES:  # Search values are case sensitive but the URL for categories is always lowercase
-            if cat.casefold() == scrape_item.url.name.casefold():
-                category_name = cat
-                break
-        if not category_name:
-            raise ScrapeError(422)
-        add_data = {"mode": category_name, "profile": None}
-        await self._generic_search_pager(scrape_item, add_data, category_name, "category")
+        ...
 
     @error_handling_wrapper
     async def tag(self, scrape_item: ScrapeItem) -> None:
-        tag_name = scrape_item.url.name
-        add_data = {"mode": "SearchMoreTag", "tag": tag_name, "profile": None}
-        await self._generic_search_pager(scrape_item, add_data, tag_name, "tag")
+        ...
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem) -> None:
@@ -141,18 +118,14 @@ class PMVHavenCrawler(Crawler):
 
         await self.process_video_info(scrape_item, info_table, info_table[video_info_idx])
 
-    async def _generic_search_pager(self, scrape_item: ScrapeItem, add_data: dict, name: str, type: str = "") -> None:
-        title: str = ""
-        api_url = API_ENTRYPOINT / "search"
-        async for json_resp in self.api_pager(api_url, add_data):
-            if not title:
-                title = f"{name} [{type}]" if type else name
-                title = self.create_title(title)
-                scrape_item.setup_as_album(title)
-
-            await self.iter_video_info(scrape_item, json_resp["data"])
-
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    @error_handling_wrapper
+    async def process_video_list(self, scrape_item: ScrapeItem, soup: BeautifulSoup) -> None:
+        info_table = json.loads(css.select_text(soup, Selectors.APP_JSON))
+        video_info_list = [data for data in info_table if isinstance(data, dict) and "videoUrl" in data]
+        for video_info in video_info_list:
+            await self.process_video_info(scrape_item, info_table, video_info)
 
     @error_handling_wrapper
     async def process_video_info(self, scrape_item: ScrapeItem, info_table: dict, video_info: dict) -> None:
@@ -175,43 +148,3 @@ class PMVHavenCrawler(Crawler):
         custom_filename = self.create_custom_filename(title, ext, file_id=video_id, resolution=resolution)
         await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename)
 
-    async def iter_video_info(self, scrape_item: ScrapeItem, videos: list[dict], new_title_part: str = "") -> None:
-        for video in videos:
-            link = _canonical_url(video)
-            new_scrape_item = scrape_item.create_child(link, new_title_part=new_title_part)
-            await self.process_video_info(new_scrape_item, video)
-            scrape_item.add_children()
-
-    async def api_pager(
-        self, api_url: AbsoluteHttpURL, add_data: dict, *, check_key: str = "data"
-    ) -> AsyncGenerator[dict]:
-        """Generator of API pages."""
-        page: int = 1
-        is_profile = api_url.name == "profileInput"
-        add_headers = {"Content-Type": "text/plain;charset=UTF-8"} if is_profile else None
-
-        for page in itertools.count(1):
-            data = {"index": page} | add_data
-            if is_profile:
-                data = json.dumps(data)
-
-            json_resp: dict[str, Any] = await self.request_json(
-                api_url,
-                method="POST",
-                data=data,
-                headers=add_headers,
-            )
-            has_videos = bool(json_resp[check_key])
-            if not has_videos:
-                break
-            yield json_resp
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-def _canonical_url(video_info: dict[str, str]) -> AbsoluteHttpURL:
-    title = video_info.get("title") or video_info["uploadTitle"]
-    video_id = video_info["_id"]
-    path = f"{title}_{video_id}"
-    return PRIMARY_URL / "video" / path
