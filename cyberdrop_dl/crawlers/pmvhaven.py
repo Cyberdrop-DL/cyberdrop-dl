a1769e38866057b5f02c7bb5dfad1a6424a24a82
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-12-10 10:13:18 -0500

    refactor: save metadata

--- cyberdrop_dl/crawlers/pmvhaven.py
+++ cyberdrop_dl/crawlers/pmvhaven.py
@@ -14,7 +14,6 @@ if TYPE_CHECKING:
 
 
 class Selectors:
-    APP_JSON = "script#__NUXT_DATA__"
     USER_NAME = "div.user-profile-card h1"
 
 
@@ -52,31 +51,26 @@ class PMVHavenCrawler(Crawler):
         scrape_item.setup_as_profile(title)
 
         nuxt_data = css.get_nuxt_data(soup)
-        await self.process_video_list(scrape_item, nuxt_data)
+        await self._process_video_list(scrape_item, nuxt_data)
 
     @error_handling_wrapper
     async def playlist(self, scrape_item: ScrapeItem) -> None:
         soup = await self.request_soup(scrape_item.url)
         nuxt_data = css.get_nuxt_data(soup)
         playlist = css.parse_nuxt_obj(nuxt_data, "playlist")
-        playlist_name = playlist["name"]
-
-        title = f"{playlist_name} [playlist]"
-        title = self.create_title(title)
+        name = playlist["name"]
+        title = self.create_title(f"{name} [playlist]")
         scrape_item.setup_as_album(title)
-
-        await self.process_video_list(scrape_item, nuxt_data)
+        await self._process_video_list(scrape_item, nuxt_data)
 
     @error_handling_wrapper
     async def search(self, scrape_item: ScrapeItem) -> None:
         soup = await self.request_soup(scrape_item.url)
         tags = scrape_item.url.query.get("tags") or scrape_item.url.query.get("musicSong")
-        title = f"{tags} [search]"
-        title = self.create_title(title)
+        title = self.create_title(f"{tags} [search]")
         scrape_item.setup_as_album(title)
-
         nuxt_data = css.get_nuxt_data(soup)
-        await self.process_video_list(scrape_item, nuxt_data)
+        await self._process_video_list(scrape_item, nuxt_data)
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem) -> None:
@@ -86,25 +80,21 @@ class PMVHavenCrawler(Crawler):
         soup = await self.request_soup(scrape_item.url)
         nuxt_data = css.get_nuxt_data(soup)
         video = css.parse_nuxt_obj(nuxt_data, "video", "uploaderVideosCount")
-        await self.process_video_info(scrape_item, video)
+        await self._process_video_info(scrape_item, video)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     @error_handling_wrapper
-    async def process_video_list(self, scrape_item: ScrapeItem, nuxt_data: list[Any]) -> None:
+    async def _process_video_list(self, scrape_item: ScrapeItem, nuxt_data: list[Any]) -> None:
         for video_info in css.parse_nuxt_objs(nuxt_data, "videoUrl"):
-            await self.process_video_info(scrape_item, video_info)
+            await self._process_video_info(scrape_item, video_info)
 
     @error_handling_wrapper
-    async def process_video_info(self, scrape_item: ScrapeItem, video_info: dict[str, Any]) -> None:
-        link_str: str = video_info["videoUrl"]
-        video_id: str = video_info["_id"]
-        resolution = video_info["height"]
-        title = video_info.get("title") or video_info["uploadTitle"]
-        link_str: str = video_info["videoUrl"]
+    async def _process_video_info(self, scrape_item: ScrapeItem, video_info: dict[str, Any]) -> None:
         scrape_item.possible_datetime = self.parse_date(video_info["uploadDate"])
-
-        link = self.parse_url(link_str)
+        link = self.parse_url(video_info["videoUrl"])
         filename, ext = self.get_filename_and_ext(link.name, assume_ext=".mp4")
-        custom_filename = self.create_custom_filename(title, ext, file_id=video_id, resolution=resolution)
-        await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename)
+        custom_filename = self.create_custom_filename(
+            video_info["title"], ext, file_id=video_info["_id"], resolution=video_info["height"]
+        )
+        await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename, metadata=video_info)
