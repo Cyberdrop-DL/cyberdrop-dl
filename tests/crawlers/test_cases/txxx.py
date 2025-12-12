31369bba99fda242218e6d6f0b18f1005327cdf1
Barbarella <barbarella6666666@protonmail.com>
2025-12-12 16:38:55 -0300

    test: adding test cases

---
+++ tests/crawlers/test_cases/txxx.py
@@ -0,0 +1,39 @@
+DOMAIN = "txxx"
+TEST_CASES = [
+    (
+        "https://tubepornclassic.com/videos/1187075/vivi-fernandez-in-aaaaa-brasileira/",
+        [
+            {
+                "url": "https://tubepornclassic.com/videos/1187075/vivi-fernandez-in-aaaaa-brasileira",
+                "filename": "Vivi Fernandez In Aaaaa Brasileira [1187075].mp4",
+                "referer": "https://tubepornclassic.com/videos/1187075/vivi-fernandez-in-aaaaa-brasileira/",
+                "download_folder": r"re:Loose Files \(TXXX\)",
+                "datetime": 1715157482,
+            }
+        ],
+    ),
+    (
+        "https://vxxx.com/video-1673601",
+        [
+            {
+                "url": "https://vxxx.com/video-1673601",
+                "filename": "Three Sweet Teens Pov Blow And Fuck - Bonnie Dolce (18+) [1673601].mp4",
+                "referer": "https://vxxx.com/video-1673601",
+                "download_folder": r"re:Loose Files \(TXXX\)",
+                "datetime": 1763849593,
+            }
+        ],
+    ),
+    (
+        "https://hotmovs.com/videos/9741037/farewell-deep-kiss/",
+        [
+            {
+                "url": "https://hotmovs.com/videos/9741037/farewell-deep-kiss",
+                "filename": "Farewell Deep Kiss [9741037].mp4",
+                "referer": "https://hotmovs.com/videos/9741037/farewell-deep-kiss",
+                "download_folder": r"re:Loose Files \(TXXX\)",
+                "datetime": 1612831801,
+            }
+        ],
+    ),
+]
