f90f5d37dee4acaaa48e5709cb92293eedf228b9
Barbarella <barbarella6666666@protonmail.com>
2025-12-12 16:42:18 -0300

    refactor: removing unused import

--- cyberdrop_dl/crawlers/pornzog.py
+++ cyberdrop_dl/crawlers/pornzog.py
@@ -8,7 +8,6 @@ from cyberdrop_dl.utils import css
 from cyberdrop_dl.utils.utilities import error_handling_wrapper
 
 if TYPE_CHECKING:
-    from cyberdrop_dl.crawlers.crawler import SupportedDomains
     from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
 
