7c4cd9e535e52b3f1cde3f8944c4c8ef6bce67e3
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-01-17 00:27:04 -0500

    refactor: updated formats preference

--- cyberdrop_dl/crawlers/bandcamp.py
+++ cyberdrop_dl/crawlers/bandcamp.py
@@ -18,7 +18,7 @@ if TYPE_CHECKING:
 
     from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
-SUPPORTED_FORMATS = "mp3-320", "aac-hi", "mp3", "flac", "vorbis", "wav", "alas", "aiff"  # Ordered by preference
+SUPPORTED_FORMATS = "mp3-320", "mp3", "aac-hi", "wav", "flac", "vorbis", "aiff", "alas"  # Ordered by preference
 USE_FORMATS = tuple(dict.fromkeys((env.BANDCAMP_FORMATS).split(","))) if env.BANDCAMP_FORMATS else SUPPORTED_FORMATS
 
 
