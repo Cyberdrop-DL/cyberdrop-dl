f02e9d95bea2701c1bf88b398f596b47969ed3cb
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-01-23 13:48:27 -0500

    tests: add album tests

--- tests/crawlers/test_cases/vsco.py
+++ tests/crawlers/test_cases/vsco.py
@@ -14,5 +14,104 @@ TEST_CASES = [
                 "datetime": 1768344414,
             }
         ],
-    )
+    ),
+    (
+        "https://vsco.co/jollyt/gallery",
+        [
+            {
+                "url": "http://vsco.co/jollyt/media/693f6a601d2d2bba406eb093",
+                "filename": "693f6a601d2d2bba406eb093.jpg",
+                "debrid_link": "https://im.vsco.co/aws-us-west-2/48de72/5979464/693f6a601d2d2bba406eb093/981efca9-7bc7-4094-838e-37614e7b0ee23057935271654318106.jpg",
+                "original_filename": "981efca9-7bc7-4094-838e-37614e7b0ee23057935271654318106.jpg",
+                "referer": "http://vsco.co/jollyt/media/693f6a601d2d2bba406eb093",
+                "album_id": None,
+                "datetime": 1765763682,
+                "download_folder": "re:jollyt (VSCO)",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/6939e9f3e0c47d7e19beac65",
+                "filename": "6939e9f3e0c47d7e19beac65.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/5d251c6de1676d2f0922e9e8",
+                "filename": "5d251c6de1676d2f0922e9e8.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/693866f862ca450b8c3d205a",
+                "filename": "693866f862ca450b8c3d205a.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/58c4a2c4631f9a55552cbeba",
+                "filename": "58c4a2c4631f9a55552cbeba.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/58a9f5657d982b7e267cf21e",
+                "filename": "58a9f5657d982b7e267cf21e.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/58a9f55c7d982b7e267cf21d",
+                "filename": "58a9f55c7d982b7e267cf21d.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/58a648767268df0c5723849b",
+                "filename": "58a648767268df0c5723849b.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/58a0cd9944251a254ecf3073",
+                "filename": "58a0cd9944251a254ecf3073.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/58a5a59403c5196354c01869",
+                "filename": "58a5a59403c5196354c01869.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/58a0a767a6884e1b07c8faca",
+                "filename": "58a0a767a6884e1b07c8faca.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/58a0a04914467b1911b46ca0",
+                "filename": "58a0a04914467b1911b46ca0.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/567dc28e66331e45358b456c",
+                "filename": "567dc28e66331e45358b456c.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/567dc26546331ebf258b456b",
+                "filename": "567dc26546331ebf258b456b.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/567dc253847e5fb71b8b456a",
+                "filename": "567dc253847e5fb71b8b456a.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/567dc223f39ccbe93e8b4575",
+                "filename": "567dc223f39ccbe93e8b4575.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/56749ca049331e60498b4574",
+                "filename": "56749ca049331e60498b4574.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/56749c736b331efc628b456f",
+                "filename": "56749c736b331efc628b456f.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/5659a73d67331e8b388b4568",
+                "filename": "5659a73d67331e8b388b4568.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/56749c472a331e76378b456d",
+                "filename": "56749c472a331e76378b456d.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/564932cfa70ef245028b456b",
+                "filename": "564932cfa70ef245028b456b.jpg",
+            },
+            {
+                "url": "http://vsco.co/jollyt/media/56493306a0ab338f098b4567",
+                "filename": "56493306a0ab338f098b4567.jpg",
+            },
+        ],
+    ),
 ]
