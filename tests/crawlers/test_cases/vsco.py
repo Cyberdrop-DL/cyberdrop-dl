0e71451d0000cf2701e23d2047201fb000f36da7
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-01-23 13:33:30 -0500

    tests: add tests

---
+++ tests/crawlers/test_cases/vsco.py
@@ -0,0 +1,18 @@
+DOMAIN = "vsco"
+TEST_CASES = [
+    (
+        "http://vsco.co/heliaabbaszadeh/media/6966cb5dff125db06d53e4ee",
+        [
+            {
+                "url": "http://vsco.co/heliaabbaszadeh/media/6966cb5dff125db06d53e4ee",
+                "referer": "http://vsco.co/heliaabbaszadeh/media/6966cb5dff125db06d53e4ee",
+                "download_folder": "re:heliaabbaszadeh (VSCO)",
+                "filename": "vsco_011326.jpg",
+                "original_filename": "vsco_011326.jpg",
+                "debrid_link": "https://im.vsco.co/aws-us-west-2/b62a22/417332/6966cb5dff125db06d53e4ee/vsco_011326.jpg",
+                "album_id": None,
+                "datetime": 1768344414,
+            }
+        ],
+    )
+]
