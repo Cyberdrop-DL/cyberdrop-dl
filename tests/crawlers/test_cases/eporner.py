e629c8cfcdf7c2641ff666125fefce029ce4b12c
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-01-25 07:33:16 -0500

    fix: encoded characters in filename

--- tests/crawlers/test_cases/eporner.py
+++ tests/crawlers/test_cases/eporner.py
@@ -1,14 +1,15 @@
 DOMAIN = "eporner"
 TEST_CASES = [
     (
-        "https://www.eporner.com/video-KND6xDDBSLN/ahegao-goon-pmv/",
+        "https://www.eporner.com/video-ia7DS1X427X/yoga-pmv/",
         [
             {
-                "url": "https://www.eporner.com/dload/KND6xDDBSLN/1080/11420026-1080p.mp4",
-                "filename": "Ahegao Goon PMV [KND6xDDBSLN][1080p].mp4",
-                "referer": "https://www.eporner.com/video-KND6xDDBSLN",
+                "url": "https://www.eporner.com/dload/ia7DS1X427X/2160/11435843-2160p-av1.mp4",
+                "filename": "Yoga PMV [ia7DS1X427X][av1][4K].mp4",
+                "original_filename": '"Yoga" PMV',
+                "referer": "https://www.eporner.com/video-ia7DS1X427X",
                 "album_id": None,
-                "datetime": None,
+                "datetime": 1722220285,
             }
         ],
     ),
