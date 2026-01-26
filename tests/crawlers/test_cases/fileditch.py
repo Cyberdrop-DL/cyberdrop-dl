16283baa18c933646b482879c4767e9e21d6d707
Barbarella <barbarella6666666@protonmail.com>
2026-01-26 14:11:32 -0300

    refactor: changing schema

--- tests/crawlers/test_cases/fileditch.py
+++ tests/crawlers/test_cases/fileditch.py
@@ -1,12 +1,12 @@
 DOMAIN = "fileditch"
 TEST_CASES = [
     (
-        "http://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
+        "https://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
         [
             {
                 "url":  r"re:https://thegumonmyshoe.me/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
                 "filename": "FrmLzfLKUHBWDTQfqaTZ.mp4",
-                "referer": "http://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
+                "referer": "https://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
                 "download_folder": r"re:Loose Files (Fileditch)",
                 "datetime": None,
             },
