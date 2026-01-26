b32945de83e3c410b829e6b9934a462b83fb7663
Barbarella <barbarella6666666@protonmail.com>
2026-01-26 14:09:48 -0300

    fix: download button new classname (fileditch)

---
+++ tests/crawlers/test_cases/fileditch.py
@@ -0,0 +1,15 @@
+DOMAIN = "fileditch"
+TEST_CASES = [
+    (
+        "http://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
+        [
+            {
+                "url":  r"re:https://thegumonmyshoe.me/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
+                "filename": "FrmLzfLKUHBWDTQfqaTZ.mp4",
+                "referer": "http://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
+                "download_folder": r"re:Loose Files (Fileditch)",
+                "datetime": None,
+            },
+        ],
+    ),
+]
