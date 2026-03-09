af1878349e497b4b032b4233d446b8e812982a53
Barbarella <barbarella6666666@protonmail.com>
2026-03-09 16:08:18 -0300

    adding test

---
+++ tests/crawlers/test_cases/filesterme.py
@@ -0,0 +1,13 @@
+DOMAIN = "filester.me"
+TEST_CASES = [
+    (
+        "https://filester.me/d/TpnIf3E",
+        [
+            {
+                "url": "https://filester.me/d/TpnIf3E",
+                "filename": "Vladi vs measuring tape (height in heels, hands and feet included).mp4",
+                "download_folder": r"re:Loose Files (Filester.me)",
+            },
+        ],
+    ),
+]
