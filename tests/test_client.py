7b79adaeac0aa13114bb0775d9077b24267d6ab2
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-27 08:31:10 -0500

    fix: special case curl import

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,10 @@
 import aiohttp
 import pytest
-from curl_cffi.requests import AsyncSession
+
+try:
+    from curl_cffi.requests import AsyncSession
+except ImportError:
+    AsyncSession = None
 
 from cyberdrop_dl.managers.client_manager import ClientManager
 from cyberdrop_dl.managers.manager import Manager
@@ -18,10 +22,12 @@ async def test_context_manager(client: ClientManager) -> None:
     with pytest.raises(AttributeError):
         _ = client._download_session
 
-    with pytest.raises(AttributeError):
-        _ = client._curl_session
+    if AsyncSession:
+        with pytest.raises(AttributeError):
+            _ = client._curl_session
 
     async with client:
         assert type(client._session) is aiohttp.ClientSession
         assert type(client._download_session) is aiohttp.ClientSession
-        assert type(client._curl_session) is AsyncSession
+        if AsyncSession:
+            assert type(client._curl_session) is AsyncSession
