3b24ef6875e46c72a69b33cc77a972f750cf74a5
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-29 08:48:12 -0500

    refactor: truncate HTML on tracebacks

---
+++ cyberdrop_dl/tracebacks.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+
+def patch() -> None:
+    from bs4.element import PageElement
+    from rich import pretty
+
+    from cyberdrop_dl.utils import truncated_preview
+
+    traverse = pretty.traverse
+    if traverse.__name__ == "new_traverse":
+        return
+
+    def is_page_element(obj: object) -> bool:
+        try:
+            return isinstance(obj, PageElement)
+        except Exception:
+            return False
+
+    def new_traverse(obj, *args, **kwargs):
+        value_repr = None
+        if is_page_element(obj):
+            try:
+                value_repr = truncated_preview(repr(obj))
+            except Exception as error:
+                value_repr = f"<repr-error {str(error)!r}>"
+
+            return pretty.Node(value_repr=value_repr, last=False)
+        return traverse(obj, *args, **kwargs)
+
+    pretty.traverse = new_traverse
+
+
+def install_except_hook() -> None:
+    patch()
+    from rich.traceback import install as install_rich_tracebacks
+
+    _ = install_rich_tracebacks(width=None, word_wrap=True, max_frames=3)
