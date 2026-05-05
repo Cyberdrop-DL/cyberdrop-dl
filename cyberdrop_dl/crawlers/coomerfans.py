5d7407b089c949d97d94e1b1852b40d0538cd27d
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-05 15:33:29 -0500

    feat: add user support

--- cyberdrop_dl/crawlers/coomerfans.py
+++ cyberdrop_dl/crawlers/coomerfans.py
@@ -22,7 +22,7 @@ _find_http_urls = re.compile(r"(?:http(?!.*\.\.)[^ ]*?)(?=($|\n|\r\n|\r|\s|\"|\[
 @dataclasses.dataclass(slots=True)
 class Post:
     date: str
-    model: str
+    model_name: str
     content: str
     title: str
     images: tuple[str, ...]
@@ -31,13 +31,25 @@ class Post:
 
 class CoomerFansCrawler(Crawler):
     SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
-        "Post": "/p/<post_id>/<user_id>/<service>",
+        "Post": "/p/<post_id>/...",
+        "User": "/u/<user_id>/...",
+        "**NOTE**": "`--ignore-coomer-post-content` affects this crawler. All other kemono config options are ignored",
     }
 
     DOMAIN: ClassVar[str] = "coomerfans"
     FOLDER_DOMAIN: ClassVar[str] = "CoomerFans"
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://coomerfans.com")
     DEFAULT_POST_TITLE_FORMAT: ClassVar[str] = "{date} - {title}"
+    NEXT_PAGE_SELECTOR: ClassVar[str] = ".pagination a.next"
+
+    @classmethod
+    def transform_url(cls, url: AbsoluteHttpURL) -> AbsoluteHttpURL:
+        url = super().transform_url(url)
+        match url.parts[1:]:
+            case ["p", _, *rest] | ["u", _, _, *rest] if not rest:
+                return url / ""
+            case _:
+                return url
 
     @property
     def ignore_content(self) -> bool:
@@ -45,20 +57,24 @@ class CoomerFansCrawler(Crawler):
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         match scrape_item.url.parts[1:]:
-            case ["p", post_id, user_id, _]:
-                return await self.post(scrape_item, post_id, user_id)
+            case ["p", post_id, *_]:
+                return await self.post(scrape_item, post_id)
+
+            case ["u", _service, _user_id, *_]:
+                return await self.profile(scrape_item)
             case _:
                 raise ValueError
 
     @error_handling_wrapper
-    async def post(self, scrape_item: ScrapeItem, post_id: str, user_id: str) -> None:
+    async def post(self, scrape_item: ScrapeItem, post_id: str) -> None:
         soup = await self.request_soup(scrape_item.url)
         post = _parse_post(soup)
-        title = self.create_title(post.model, user_id)
-        scrape_item.setup_as_album(title, album_id=user_id)
+        scrape_item.setup_as_album(self.create_title(post.model_name))
         scrape_item.uploaded_at = date = self.parse_iso_date(post.date)
         post_title = self.create_separate_post_title(post.title, post_id, date)
         scrape_item.add_to_parent_title(post_title)
+        self.create_task(self.write_metadata(scrape_item, f"post_{post_id}", post))
+        self._post(scrape_item, post)
 
     def _post(self, scrape_item: ScrapeItem, post: Post) -> None:
         seen: set[str] = set()
@@ -68,16 +84,11 @@ class CoomerFansCrawler(Crawler):
                 self.create_task(self.direct_file(scrape_item, self.parse_url(url)))
                 scrape_item.add_children()
 
-        self._handle_post_content(scrape_item, post)
-
-    def _handle_post_content(self, scrape_item: ScrapeItem, post: Post) -> None:
-        """Gets links out of content in post and sends them to a new crawler."""
         if not post.content or self.ignore_content:
             return
 
         for url in self.__parse_content_urls(post):
-            new_scrape_item = scrape_item.create_child(url)
-            self.handle_external_links(new_scrape_item)
+            self.handle_external_links(scrape_item.create_child(url))
             scrape_item.add_children()
 
     def __parse_content_urls(self, post: Post) -> Generator[AbsoluteHttpURL]:
@@ -93,6 +104,12 @@ class CoomerFansCrawler(Crawler):
                     if self.DOMAIN not in url.host:
                         yield url
 
+    @error_handling_wrapper
+    async def profile(self, scrape_item: ScrapeItem) -> None:
+        async for soup in self.web_pager(scrape_item.url):
+            for _, child in self.iter_children(scrape_item, soup, "a.view-post[href]"):
+                self.create_task(self.run(child))
+
 
 def _parse_post(soup: BeautifulSoup) -> Post:
     main = css.select(soup, "main.content")
@@ -103,7 +120,7 @@ def _parse_post(soup: BeautifulSoup) -> Post:
 
     return Post(
         title=get("h1"),
-        model=get(".model-name"),
+        model_name=get(".model-name"),
         content=get(".post-date + p"),
         date=extr_text(get(".post-date"), "Added ", " +"),
         images=tuple(css.iselect(body, "img", "src")),
