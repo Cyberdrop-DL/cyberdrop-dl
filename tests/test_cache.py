c12b893fd54bec5f3cc067e90e29ba7d19bbbd82
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-06 11:01:23 -0500

    tests: update cache tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+from typing import Any
+
 from cyberdrop_dl import __version__, yaml
-from cyberdrop_dl.managers.manager import Manager
+from cyberdrop_dl.managers.manager import Manager, _cache_context
 
 
 def test_cache_file_is_not_saved_outside_ctx(manager: Manager) -> None:
@@ -10,11 +13,12 @@ def test_cache_file_is_not_saved_outside_ctx(manager: Manager) -> None:
     assert not cache_file.exists()
 
 
-async def test_cache_file_is_saved_in_ctx(manager: Manager) -> None:
-    cache_file = manager.appdata.cache_file
-    async with manager:
-        manager.cache["test"] = 1
-        assert manager.cache == {"test": 1}
+def test_cache_file_is_saved_in_ctx(tmp_path: Path) -> None:
+    cache_file = tmp_path / "cache_file.txt"
+    cache: dict[str, Any] = {}
+    with _cache_context(cache_file, cache):
+        cache["test"] = 1
+        assert cache == {"test": 1}
         assert cache_file.is_file()
 
     assert cache_file.is_file()
