d19c4bb2cc4a7ae041ebc9865a37443828b21c13
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-07 16:04:06 -0500

    tests: update client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,6 @@
 import aiohttp
 import pytest
-
-try:
-    from curl_cffi.requests import AsyncSession
-except ImportError:
-    AsyncSession = None
+from curl_cffi.requests import AsyncSession
 
 from cyberdrop_dl.manager import Manager
 from cyberdrop_dl.managers.client_manager import ClientManager
@@ -22,11 +18,10 @@ async def test_context_manager(client: ClientManager) -> None:
     with pytest.raises(AttributeError):
         _ = client._download_session
 
-    with pytest.raises(AttributeError):
-        _ = client.curl_session
+    assert client._curl_session is None
 
     async with client:
         assert type(client._session) is aiohttp.ClientSession
         assert type(client._download_session) is aiohttp.ClientSession
-        if AsyncSession:
-            assert type(client.curl_session) is AsyncSession
+        assert client._curl_session is None
+        assert type(client.curl_session) is AsyncSession
