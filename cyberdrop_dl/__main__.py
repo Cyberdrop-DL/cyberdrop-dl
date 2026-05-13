97eca0e54588a8b2717b89f0b691348c598244df
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-12 22:06:57 -0500

    refactor: rename entrypoint

--- cyberdrop_dl/__main__.py
+++ cyberdrop_dl/__main__.py
@@ -9,7 +9,7 @@ from cyberdrop_dl.cli import app
 tracebacks.install_exception_hook()
 
 
-def main(args: Sequence[str] | None = None) -> int | None:
+def run_cdl(args: Sequence[str] | None = None) -> int | None:
     from cyberdrop_dl.logs import setup_console_logging
 
     with setup_console_logging():
@@ -21,5 +21,9 @@ def main(args: Sequence[str] | None = None) -> int | None:
             app.console.print(CycloptsPanel(msg, title=exc_group.message))
 
 
+def main(args: Sequence[str] | None = None):
+    sys.exit(bool(run_cdl(args)))
+
+
 if __name__ == "__main__":
-    sys.exit(bool(main()))
+    main()
