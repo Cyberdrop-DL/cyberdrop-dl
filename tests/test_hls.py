6c9c9cdfc126ab438d32023d340d3a312c8e4f73
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-13 13:56:00 -0500

    tests: add tests for output path

--- tests/test_hls.py
+++ tests/test_hls.py
@@ -4,6 +4,37 @@ from m3u8.model import Segment
 
 from cyberdrop_dl.downloader import hls
 from cyberdrop_dl.url_objects import AbsoluteHttpURL, MediaItem
+from cyberdrop_dl.utils.m3u8 import M3U8
+
+VARIANT_M3U8_CONTENT = """
+#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-TARGETDURATION:10
+#EXT-X-MEDIA-SEQUENCE:0
+
+#EXTINF:10.000,
+media-segment-00001.ts
+#EXTINF:10.000,
+media-segment-00002.ts
+#EXTINF:8.500,
+media-segment-00003.ts
+#EXT-X-ENDLIST
+"""
+
+VARIANT_M3U8_CONTENT_SUBS = """
+#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-TARGETDURATION:10
+#EXT-X-MEDIA-SEQUENCE:0
+
+#EXTINF:10.000,
+media-segment-00001.vtt
+#EXTINF:10.000,
+media-segment-00002.ts
+#EXTINF:8.500,
+media-segment-00003.ts
+#EXT-X-ENDLIST
+"""
 
 
 def test_parse_segments() -> None:
@@ -48,3 +79,16 @@ def test_create_media_segments() -> None:
     assert seg_item.headers == item.headers
     assert seg_item.headers is not item.headers
     assert seg_item.ext == item.ext
+
+
+def test_prepare_output_path(tmp_path: Path) -> None:
+    base_uri = AbsoluteHttpURL("https://www.example.com")
+    m3u8 = M3U8(content=VARIANT_M3U8_CONTENT, media_type="video", base_uri=base_uri)
+    output = tmp_path / "download" / "m3u8.mp4"
+    file = hls._prepare_output_path(m3u8, output)
+    assert file == tmp_path / "download" / "m3u8.video.ts"
+
+    m3u8 = M3U8(content=VARIANT_M3U8_CONTENT_SUBS, media_type="subtitle", base_uri=base_uri)
+    output = tmp_path / "download" / "m3u8.txt"
+    file = hls._prepare_output_path(m3u8, output)
+    assert file == tmp_path / "download" / "m3u8.subtitle.vtt"
