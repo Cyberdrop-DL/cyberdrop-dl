fdb16a9020487bc0c89080bbb5500ab6318a8448
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-17 22:49:01 -0500

    fix SIM103

--- cyberdrop_dl/url_objects.py
+++ cyberdrop_dl/url_objects.py
@@ -429,9 +429,7 @@ class QueryDatetimeRange(NamedTuple):
         return self
 
     def is_in_range(self, other: datetime.datetime) -> bool:
-        if (self.before and other >= self.before) or (self.after and other <= self.after):
-            return False
-        return True
+        return not (self.before and other >= self.before or self.after and other <= self.after)
 
     def as_query(self) -> dict[str, Any]:
         return {name: value.isoformat() for name, value in self._asdict().items() if value}
