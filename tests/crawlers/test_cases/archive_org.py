a53b1796af8a2b238b724548440e5c59b966dd41
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-21 11:44:38 -0500

    test: add test case

---
+++ tests/crawlers/test_cases/archive_org.py
@@ -0,0 +1,19 @@
+DOMAIN = "archive_org"
+TEST_CASES = [
+    {
+        "url": "https://archive.org/details/science-fiction-theatre-season-2/2-05+When+a+Camera+Fails.mp4",
+        "results": [
+            {
+                "url": "https://archive.org/download/science-fiction-theatre-season-2/2-05%20When%20a%20Camera%20Fails.mp4",
+                "filename": "2-05 When a Camera Fails.mp4",
+                "debrid_link": None,
+                "original_filename": "2-05 When a Camera Fails.mp4",
+                "referer": "https://archive.org/details/science-fiction-theatre-season-2/2-05%20When%20a%20Camera%20Fails.mp4",
+                "album_id": None,
+                "uploaded_at": 1710303585,
+                "download_folder": "re:Science Fiction Theatre season 2 eps 1-13 (1956) colorized (Archive.org)",
+            }
+        ],
+        "count": 1,
+    }
+]
