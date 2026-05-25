5abdc26ef0f09f7fd049322becd579b28a4cdac8
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-25 01:50:23 -0500

    feat: add soundgasm support

---
+++ tests/crawlers/test_cases/soundgasm.py
@@ -0,0 +1,19 @@
+DOMAIN = "soundgasm"
+TEST_CASES = [
+    {
+        "url": "https://soundgasm.net/u/beegeewanders/Reading-Your-Shy-Besties-Mind",
+        "results": [
+            {
+                "url": "https://media.soundgasm.net/sounds/0211aae94398cdcb519bff05f6591ae5d6bef135.m4a",
+                "filename": "Reading Your Shy Besties Mind.m4a",
+                "debrid_link": None,
+                "original_filename": "Reading Your Shy Besties Mind.m4a",
+                "referer": "https://soundgasm.net/u/beegeewanders/Reading-Your-Shy-Besties-Mind",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:beegeewanders (Soundgasm)",
+            }
+        ],
+        "count": 1,
+    }
+]
