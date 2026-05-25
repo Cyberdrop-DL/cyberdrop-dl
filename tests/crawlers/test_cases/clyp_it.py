e0a4e7c1b755c45ad2f0aea2e71c03dff92c3c3d
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-25 14:51:42 -0500

    feat: add clyp.it support

---
+++ tests/crawlers/test_cases/clyp_it.py
@@ -0,0 +1,31 @@
+DOMAIN = "clyp.it"
+TEST_CASES = [
+    {
+        "url": "https://clyp.it/x1ujleej",
+        "results": [
+            {
+                "url": "re:https://audio.clyp.it/x1ujleej.wav?",
+                "filename": "Monkeys favourite songs MASHUP! [x1ujleej].wav",
+                "debrid_link": None,
+                "original_filename": "Monkey's favourite songs MASHUP!",
+                "referer": "https://clyp.it/x1ujleej",
+                "album_id": None,
+                "uploaded_at": 1684226059,
+                "download_folder": "re:Valentin Brunn (Clyp.it)",
+            }
+        ],
+        "count": 1,
+    },
+    {
+        "url": "https://clyp.it/user/gxslmwpj",
+        "results": [
+            {
+                "url": "rehttps://audio.clyp.it/",
+                "album_id": None,
+                "uploaded_at": int,
+                "download_folder": "re:Valentin Brunn (Clyp.it)",
+            },
+        ],
+        "count": 239,
+    },
+]
