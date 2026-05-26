998d49c83823ab82bf3aa9c63add39120fb3c0a5
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-26 16:14:59 -0500

    test: re-enable http tests w onepace

--- tests/next_js/test_next_js.py
+++ tests/next_js/test_next_js.py
@@ -1,9 +1,16 @@
-from pathlib import Path
+from __future__ import annotations
 
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import aiohttp
 import pytest
 from bs4 import BeautifulSoup
 
 from cyberdrop_dl.utils import next_js
+
+if TYPE_CHECKING:
+    from collections.abc import AsyncGenerator
 
 TEST_HTML = (Path(__file__).parent / "nextjsv13.html").read_text()
 soup = BeautifulSoup(TEST_HTML, "html.parser")
@@ -42,3 +49,30 @@ def test_parse() -> None:
     assert len(chunks) == 111
     for value in chunks.values():
         assert value != next_js._MagicString.ERROR
+
+
+@pytest.fixture(name="soup", scope="module")
+async def onepace_soup() -> AsyncGenerator[BeautifulSoup]:
+    async with aiohttp.ClientSession() as session:
+        resp = await session.get("https://onepace.net/en/watch")
+        yield BeautifulSoup(await resp.text(), "html.parser")
+
+
+def test_next_js_parser(soup: BeautifulSoup) -> None:
+    next_data = next_js.extract(soup)
+    assert isinstance(next_data, dict)
+    assert next_data["1"] == "Sreact.fragment"
+    assert len(next_data) > 10
+
+
+def test_next_js_find(soup: BeautifulSoup) -> None:
+    next_data = next_js.extract(soup)
+    episode_keys = "slug", "title", "playlistGroups"
+    ep = next_js.find(next_data, *episode_keys)
+    assert isinstance(ep, dict)
+    assert set(episode_keys).issubset(ep.keys())
+    all_episodes = list(next_js.ifind(next_data, *episode_keys))
+    assert len(all_episodes) > 76
+    for ep in all_episodes:
+        assert isinstance(ep, dict)
+        assert set(episode_keys).issubset(ep.keys())
