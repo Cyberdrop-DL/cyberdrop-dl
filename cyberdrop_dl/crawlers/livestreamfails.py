1cc1d57a4337fe27f0bcd9f54bb455d24d14d2cd
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-26 21:35:19 -0500

    feat: add streamer support

--- cyberdrop_dl/crawlers/livestreamfails.py
+++ cyberdrop_dl/crawlers/livestreamfails.py
@@ -3,11 +3,13 @@ from __future__ import annotations
 import dataclasses
 from typing import TYPE_CHECKING, Any, ClassVar
 
-from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths
+from cyberdrop_dl.crawlers.crawler import API, Crawler, RateLimit, SupportedPaths
 from cyberdrop_dl.url_objects import AbsoluteHttpURL
 from cyberdrop_dl.utils import deserialize, error_handling_wrapper
 
 if TYPE_CHECKING:
+    from collections.abc import AsyncGenerator
+
     from cyberdrop_dl.url_objects import ScrapeItem
 
 
@@ -16,11 +18,56 @@ class _CDN:
     VIDEO = AbsoluteHttpURL("https://livestreamfails-video-prod.b-cdn.net/video")
 
 
-_API_ENTRYPOINT = AbsoluteHttpURL("https://api.livestreamfails.com")
+class LivestreamFailsCrawler(Crawler):
+    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
+        "Clip": "/clip/<video_id>",
+        "Streamer": "/streamer/<streamer_id>",
+    }
+    DOMAIN: ClassVar[str] = "livestreamfails.com"
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://livestreamfails.com")
+    _RATE_LIMIT: ClassVar[RateLimit] = 8, 1
+
+    def __post_init__(self) -> None:
+        self.api: LivestreamFailsAPI = LivestreamFailsAPI(self)
+
+    async def fetch(self, scrape_item: ScrapeItem) -> None:
+        match scrape_item.url.parts[1:]:
+            case ["clip", clip_id]:
+                return await self.clip(scrape_item, clip_id)
+            case ["streamer", streamer_id]:
+                return await self.streamer(scrape_item, streamer_id)
+            case _:
+                raise ValueError
+
+    @error_handling_wrapper
+    async def streamer(self, scrape_item: ScrapeItem, streamer_id: str) -> None:
+        scrape_item.setup_as_profile("")
+        async for videos in self.api.streamer_clips(streamer_id):
+            for video in videos:
+                url = self.PRIMARY_URL / f"clip/{video.id}"
+                new_item = scrape_item.create_child(url)
+                self.create_task(self._clip(new_item, video))
+                scrape_item.add_children()
+
+    @error_handling_wrapper
+    async def clip(self, scrape_item: ScrapeItem, clip_id: str) -> None:
+        if await self.check_complete_from_referer(scrape_item.url):
+            return
+
+        video = await self.api.clip(clip_id)
+        await self._clip(scrape_item, video)
+
+    @error_handling_wrapper
+    async def _clip(self, scrape_item: ScrapeItem, clip: Clip) -> None:
+        scrape_item.setup_as_album(self.create_title(clip.streamer.label, str(clip.streamer.id)))
+        scrape_item.uploaded_at = self.parse_iso_date(clip.createdAt)
+        _, ext = self.get_filename_and_ext(clip.src.name)
+        filename = self.create_custom_filename(clip.label, ext, file_id=str(clip.id))
+        await self.handle_file(clip.src, scrape_item, clip.label, ext, custom_filename=filename, metadata=clip)
 
 
 @dataclasses.dataclass(slots=True)
-class Video:
+class Clip:
     id: int
     label: str
     createdAt: str  # noqa: N815
@@ -35,37 +82,34 @@ class Streamer:
     label: str
 
 
-class LivestreamFailsCrawler(Crawler):
-    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Video": ("/clip/<video_id>",)}
-    DOMAIN: ClassVar[str] = "livestreamfails.com"
-    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://livestreamfails.com")
+class LivestreamFailsAPI(API):
+    ENTRYPOINT: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://api.livestreamfails.com")
 
-    async def fetch(self, scrape_item: ScrapeItem) -> None:
-        match scrape_item.url.parts[1:]:
-            case ["clip", video_id]:
-                return await self.video(scrape_item, video_id)
-            case _:
-                raise ValueError
+    async def clip(self, clip_id: str) -> Clip:
+        api_url = self.ENTRYPOINT / "clip" / clip_id
+        video: dict[str, Any] = await self.request_json(api_url)
+        return _parse_video(video)
 
-    @error_handling_wrapper
-    async def video(self, scrape_item: ScrapeItem, video_id: str) -> None:
-        if await self.check_complete_from_referer(scrape_item.url):
-            return
+    def streamer_clips(self, streamer_id: str) -> AsyncGenerator[map[Clip]]:
+        api_url = self.ENTRYPOINT / "streamer" / streamer_id / "clips"
+        return self._pager(api_url)
 
-        video = await self._request_video(video_id)
-        scrape_item.setup_as_album(self.create_title(video.streamer.label, str(video.streamer.id)))
-        scrape_item.uploaded_at = self.parse_iso_date(video.createdAt)
-        _, ext = self.get_filename_and_ext(video.src.name)
-        filename = self.create_custom_filename(video.label, ext, file_id=video_id)
-        await self.handle_file(video.src, scrape_item, video.label, ext, custom_filename=filename, metadata=video)
+    async def _pager(self, url: AbsoluteHttpURL) -> AsyncGenerator[map[Clip]]:
+        url = url.update_query(querySort="new")
+        while True:
+            resp = await self.request_json(url)
+            last_id: str = resp[-1]["id"]
+            yield map(_parse_video, resp)
+            if len(resp) < 20:
+                break
+            url = url.update_query(queryAfter=last_id)
 
-    async def _request_video(self, video_id: str) -> Video:
-        api_url = _API_ENTRYPOINT / "clip" / video_id
-        resp: dict[str, Any] = await self.request_json(api_url)
-        return deserialize(
-            Video,
-            resp,
-            src=_CDN.VIDEO / resp["videoId"],
-            thumbnail=_CDN.IMAGE / resp["imageId"],
-            streamer=deserialize(Streamer, resp["streamer"]),
-        )
+
+def _parse_video(resp: dict[str, Any]) -> Clip:
+    return deserialize(
+        Clip,
+        resp,
+        src=_CDN.VIDEO / resp["videoId"],
+        thumbnail=_CDN.IMAGE / resp["imageId"],
+        streamer=deserialize(Streamer, resp["streamer"]),
+    )
