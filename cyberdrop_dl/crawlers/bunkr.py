6cb79407aaf59c859b168674f4ab063869bf27ca
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-29 07:59:16 -0500

    refactor: handle more URL formats

--- cyberdrop_dl/crawlers/bunkr.py
+++ cyberdrop_dl/crawlers/bunkr.py
@@ -43,6 +43,7 @@ class BunkrCrawler(Crawler, db_path="name"):
         "File": (
             "/f/<slug>",
             "/d/<slug>",
+            "/i/<slug>",
         ),
         "Stream redirect": "/<slug>",
     }
@@ -52,6 +53,16 @@ class BunkrCrawler(Crawler, db_path="name"):
     _RATE_LIMIT: ClassVar[RateLimit] = 5, 1
     _USE_DOWNLOAD_SERVERS_LOCKS: ClassVar[bool] = True
     _known_good_host: ClassVar[str | None] = None
+
+    @classmethod
+    @override
+    def transform_url(cls, url: AbsoluteHttpURL) -> AbsoluteHttpURL:
+        url = super().transform_url(url)
+        match url.parts[1:]:
+            case ["v" | "d" | "i", js_slug]:
+                return url.origin() / "f" / js_slug
+            case _:
+                return url
 
     def __post_init__(self) -> None:
         self.api: BunkrAPI = BunkrAPI(self)
@@ -64,7 +75,7 @@ class BunkrCrawler(Crawler, db_path="name"):
                 return await self.reinforced_file(scrape_item, file_id)
             case ["a", album_id]:
                 return await self.album(scrape_item, album_id)
-            case ["v" | "d", _]:
+            case ["v" | "d" | "i", _]:
                 return await self.follow_redirect(scrape_item)
             case ["f", _]:
                 return await self.file(scrape_item)
@@ -261,4 +272,5 @@ def fix_db_referer(referer: str) -> str:
     url = AbsoluteHttpURL(referer)
     if BunkrCrawler.is_subdomain(url):
         return str(url)
-    return str(url.with_host(BunkrCrawler.PRIMARY_URL.host))
+
+    return str(BunkrCrawler.transform_url(url).with_host(BunkrCrawler.PRIMARY_URL.host))
