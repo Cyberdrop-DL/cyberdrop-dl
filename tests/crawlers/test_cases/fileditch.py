cdd233dbbe446619b330168b5f50bf59bf07b5f4
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-06-09 14:22:53 -0500

    feat: support beta URLs (fileditch)

--- tests/crawlers/test_cases/fileditch.py
+++ tests/crawlers/test_cases/fileditch.py
@@ -24,4 +24,20 @@ TEST_CASES = [
             }
         ],
     },
+    {
+        "url": "https://fileditchfiles.me/beta5/a292619a708980582542/%5B8.11%5D_valk1.mp4",
+        "results": [
+            {
+                "url": "https://donotsharethesetemplinksyouidiot.st/beta5/a292619a708980582542/%5B8.11%5D_valk1.mp4?md5=WBrfgN6YCxEBPQGXaJqaeA&expires=1781110076",
+                "filename": "[8.11]_valk1.mp4",
+                "debrid_link": None,
+                "original_filename": "[8.11]_valk1.mp4",
+                "referer": "https://fileditchfiles.me/beta5/a292619a708980582542/%5B8.11%5D_valk1.mp4",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:Loose Files (Fileditch)",
+            }
+        ],
+        "count": 1,
+    },
 ]
