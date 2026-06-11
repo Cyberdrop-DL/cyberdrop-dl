0e3923092c66f54537e2c704ae5e9ab8b30553db
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-06-11 04:26:01 -0500

    refactor: check for pip on uninstall

--- scripts/release/linux-macOS_remove.sh
+++ scripts/release/linux-macOS_remove.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-pip uninstall cyberdrop-dl
-pip uninstall cyberdrop-dl-patched
+if command -v pip >/dev/null 2>&1; then
+    pip uninstall cyberdrop-dl
+    pip uninstall cyberdrop-dl-patched
+fi
 uv tool uninstall cyberdrop-dl-patched
 uv cache clean
--- scripts/release/windows_remove.bat
+++ scripts/release/windows_remove.bat
@@ -1,6 +1,9 @@
 @echo off
-pip uninstall cyberdrop-dl
-pip uninstall cyberdrop-dl-patched
+where pip >nul 2>&1
+if %errorlevel%==0 (
+    pip uninstall cyberdrop-dl
+    pip uninstall cyberdrop-dl-patched
+)
 uv tool uninstall cyberdrop-dl-patched
 uv cache clean
 pause
