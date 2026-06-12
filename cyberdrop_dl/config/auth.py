9332ba92d8c09810d1f9fd16d465d075e753605f
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-06-12 12:08:36 -0500

    refactor: remove coomer and kemono session auth

--- cyberdrop_dl/config/auth.py
+++ cyberdrop_dl/config/auth.py
@@ -19,16 +19,10 @@ class JDownloaderAuth(AliasModel):
     device: str = ""
 
 
-class KemonoAuth(AliasModel):
-    session: str = ""
-
-
 @Parameter(show=False)
 class AuthSettings(AliasModel):
-    coomer: KemonoAuth = Field(default_factory=KemonoAuth)
     gofile: ApiKeyAuth = Field(default_factory=ApiKeyAuth)
     jdownloader: JDownloaderAuth = Field(default_factory=JDownloaderAuth)
-    kemono: KemonoAuth = Field(default_factory=KemonoAuth)
     meganz: EmailAuth = Field(default_factory=EmailAuth)
     pixeldrain: ApiKeyAuth = Field(default_factory=ApiKeyAuth)
     realdebrid: ApiKeyAuth = Field(default_factory=ApiKeyAuth)
