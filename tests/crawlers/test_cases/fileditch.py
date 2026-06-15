1d2181c33a4fe2aebd612ef54010f503063028fb
Barbarella <barbarella6666666@protonmail.com>
2026-06-15 14:16:23 -0300

    fix: fixing all test_cases

--- tests/crawlers/test_cases/fileditch.py
+++ tests/crawlers/test_cases/fileditch.py
@@ -4,7 +4,7 @@ TEST_CASES = [
         "url": "https://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
         "results": [
             {
-                "url": "re:thegumonmyshoe.me/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
+                "url": "re:https://donotsharethesetemplinksyouidiot.st/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
                 "filename": "FrmLzfLKUHBWDTQfqaTZ.mp4",
                 "referer": "https://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
                 "download_folder": "re:Loose Files (Fileditch)",
@@ -17,7 +17,7 @@ TEST_CASES = [
         "description": "multiple slashes on URL",
         "results": [
             {
-                "url": "re:thegumonmyshoe.me/b70/jRuBeGZlRoBWPurUARg.mp4",
+                "url": "re:https://donotsharethesetemplinksyouidiot.st/b70/jRuBeGZlRoBWPurUARg.mp4",
                 "referer": "https://fileditchfiles.me/file.php?f=/b70/jRuBeGZlRoBWPurUARg.mp4",
                 "download_folder": "re:Loose Files (Fileditch)",
                 "uploaded_at": None,
@@ -28,9 +28,8 @@ TEST_CASES = [
         "url": "https://fileditchfiles.me/beta5/a292619a708980582542/%5B8.11%5D_valk1.mp4",
         "results": [
             {
-                "url": "https://donotsharethesetemplinksyouidiot.st/beta5/a292619a708980582542/%5B8.11%5D_valk1.mp4?md5=WBrfgN6YCxEBPQGXaJqaeA&expires=1781110076",
+                "url": "re:https://donotsharethesetemplinksyouidiot.st/beta5/a292619a708980582542/%5B8.11%5D_valk1.mp4",
                 "filename": "[8.11]_valk1.mp4",
-                "debrid_link": None,
                 "original_filename": "[8.11]_valk1.mp4",
                 "referer": "https://fileditchfiles.me/beta5/a292619a708980582542/%5B8.11%5D_valk1.mp4",
                 "album_id": None,
@@ -44,9 +43,8 @@ TEST_CASES = [
         "url": "https://fileditchfiles.me/temp/d9dc8e3cc669f9d24b16/3840x2839_b545565c05a233584ef041f248a38426.jpg",
         "results": [
             {
-                "url": "https://tempfileditchyall.me/temp/d9dc8e3cc669f9d24b16/3840x2839_b545565c05a233584ef041f248a38426.jpg?md5=JVoo-HxiBOROUtbiyfX4zg&expires=1781110320",
+                "url": "re:https://tempfileditchyall.me/temp/d9dc8e3cc669f9d24b16/3840x2839_b545565c05a233584ef041f248a38426.jpg",
                 "filename": "3840x2839_b545565c05a233584ef041f248a38426.jpg",
-                "debrid_link": None,
                 "original_filename": "3840x2839_b545565c05a233584ef041f248a38426.jpg",
                 "referer": "https://fileditchfiles.me/temp/d9dc8e3cc669f9d24b16/3840x2839_b545565c05a233584ef041f248a38426.jpg",
                 "album_id": None,
