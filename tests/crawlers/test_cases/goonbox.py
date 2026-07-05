630be0cdeebe0a64a38ceb94b11ee653e198ee6a
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-05 13:26:27 -0500

    tests: add test case

---
+++ tests/crawlers/test_cases/goonbox.py
@@ -0,0 +1,35 @@
+DOMAIN = "goonbox"
+TEST_CASES = [
+    {
+        "url": "https://goonbox.cr/img/tgs7KfH",
+        "results": [
+            {
+                "url": "https://simp6.cuckcapital.cr/images4/098c105b-0c36-4fc5-afc7-78279e3bb9e7.png",
+                "filename": "098c105b-0c36-4fc5-afc7-78279e3bb9e7.png",
+                "debrid_url": None,
+                "original_filename": "098c105b-0c36-4fc5-afc7-78279e3bb9e7.png",
+                "referer": "https://goonbox.cr/img/tgs7KfH",
+                "album_id": None,
+                "uploaded_at": 1782580570,
+                "download_folder": "re:Loose Files (GoonBox)",
+            }
+        ],
+        "count": 1,
+    },
+    {
+        "url": "https://goonbox.cr/a/YVuDPn",
+        "results": [
+            {
+                "url": "https://simp6.cuckcapital.cr/images3/Screenshot_20250419_222405_Adblock-Browser3ce42a12ed2e3edf.jpg",
+                "filename": "Screenshot_20250419_222405_Adblock-Browser3ce42a12ed2e3edf.jpg",
+                "debrid_url": None,
+                "original_filename": "Screenshot_20250419_222405_Adblock-Browser3ce42a12ed2e3edf.jpg",
+                "referer": "https://goonbox.cr/img/aySgGc6",
+                "album_id": "YVuDPn",
+                "uploaded_at": 1745080005,
+                "download_folder": "re:cpiexo (GoonBox)",
+            }
+        ],
+        "count": 1,
+    },
+]
