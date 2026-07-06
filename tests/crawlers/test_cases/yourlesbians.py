9af2b0eb7756b763acbb8ba00b36d0c02b2434a0
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-05 19:10:01 -0500

    tests: update tests

--- tests/crawlers/test_cases/yourlesbians.py
+++ tests/crawlers/test_cases/yourlesbians.py
@@ -6,7 +6,7 @@ TEST_CASES = [
             {
                 "url": "https://yourlesbians.com/video/girlsway-angela-white-girlsway-newest-editor",
                 "filename": "GirlsWay - Angela White - Girlsway Newest Editor [8048][720p].mp4",
-                "debrid_link": "re:https://yourlesbians.com/get_file/7/845eb2c99dc5bced8c9159de0f9fc352/8000/8048/8048_720p.mp4",
+                "debrid_url": "re:https://yourlesbians.com/get_file/7/845eb2c99dc5bced8c9159de0f9fc352/8000/8048/8048_720p.mp4",
                 "original_filename": "8048_720p.mp4",
                 "referer": "https://yourlesbians.com/video/girlsway-angela-white-girlsway-newest-editor",
                 "album_id": None,
@@ -20,10 +20,7 @@ TEST_CASES = [
         "url": "https://yourlesbians.com/album/nicolle-florez-nude",
         "results": [
             {
-                "url": "re:https://yourlesbians.com/get_image/2/",
-                "filename": "5812.jpg",
-                "debrid_link": None,
-                "original_filename": "5812.jpg",
+                "url": "re:https://yourlesbians.com/get_image",
                 "referer": "https://yourlesbians.com/album/nicolle-florez-nude",
                 "album_id": None,
                 "uploaded_at": None,
