ad10160047c57c5d5ce9b64953190f9281c0d73b
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-07 18:04:47 -0500

    tests: update tests

--- tests/crawlers/test_cases/multporn_net.py
+++ tests/crawlers/test_cases/multporn_net.py
@@ -1,20 +1,19 @@
-DOMAIN = "multporn_net"
+DOMAIN = "multporn.net"
 TEST_CASES = [
     {
         "url": "https://multporn.net/comics/lambs_respite_2_in_piltover",
         "results": [
             {
-                "url": "https://multporn.net/sites/default/files/comics/league_of_legends/strong_bana_lamb039s_respite_2_in_piltover/00_00.png",
+                "url": "re:https://multporn.net/sites/default/files/comics/league_of_legends/strong_bana_lamb039s_respite_2_in_piltover/",
                 "filename": "00_00.png",
-                "debrid_link": None,
-                "original_filename": "00_00.png",
+                "debrid_url": None,
                 "referer": "https://multporn.net/comics/lambs_respite_2_in_piltover",
                 "album_id": None,
                 "uploaded_at": 1774805880,
                 "download_folder": "re:Lamb-s Respite 2- In Piltover (Multporn.net)",
             },
         ],
-        "count": 18,
+        "count": 21,
     },
     {
         "url": "https://multporn.net/video/ahsoka_blowjob",
@@ -22,7 +21,7 @@ TEST_CASES = [
             {
                 "url": "https://multporn.net/sites/default/files/videos/redmoa_ahsoka_-_blowjob.mp4",
                 "filename": "Ahsoka - Blowjob.mp4",
-                "debrid_link": None,
+                "debrid_url": None,
                 "original_filename": "Ahsoka - Blowjob",
                 "referer": "https://multporn.net/video/ahsoka_blowjob",
                 "album_id": None,
