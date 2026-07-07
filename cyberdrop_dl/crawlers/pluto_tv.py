0096045003fc6f3f4b7cc2767a7bbcfb28ff06a6
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-07 07:57:10 -0500

    refactor: use contextvar for stitcher

--- cyberdrop_dl/crawlers/pluto_tv.py
+++ cyberdrop_dl/crawlers/pluto_tv.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 import dataclasses
 import uuid
+from contextvars import ContextVar
 from typing import TYPE_CHECKING, Any, ClassVar, TypedDict
 
 from cyberdrop_dl.cache import cached_method
 from cyberdrop_dl.crawlers.crawler import API, Crawler, SupportedPaths
 from cyberdrop_dl.exceptions import ScrapeError
 from cyberdrop_dl.url_objects import AbsoluteHttpURL, ScrapeItem
-from cyberdrop_dl.utils import css, parse_url
+from cyberdrop_dl.utils import css
 from cyberdrop_dl.utils.dataclass import deserialize
 from cyberdrop_dl.utils.errors import error_handling_wrapper
 
@@ -44,7 +45,7 @@ class PlutoCrawler(Crawler):
 
     @error_handling_wrapper
     async def series(self, scrape_item: ScrapeItem, series_id: str, episode_id: str | None = None) -> None:
-        boot, series = await self.api.series(series_id)
+        series = await self.api.series(series_id)
         scrape_item.setup_as_album(self.create_title(series.name, series.id))
         self.create_eager_task(self.write_metadata(scrape_item, series.id, series))
         if episode_id:
@@ -53,20 +54,20 @@ class PlutoCrawler(Crawler):
             except StopIteration:
                 raise ScrapeError(404) from None
 
-            return await self._episode(scrape_item, episode, boot)
+            return await self._episode(scrape_item, episode)
 
         for ep in series.episodes():
             base_url = scrape_item.url / "episode" / ep.id
             new_item = scrape_item.create_child(base_url)
-            self.create_task(self._episode(new_item, ep, boot))
+            self.create_task(self._episode(new_item, ep))
             scrape_item.add_children()
 
     @error_handling_wrapper
-    async def _episode(self, scrape_item: ScrapeItem, ep: Episode, boot: Stitcher) -> None:
+    async def _episode(self, scrape_item: ScrapeItem, ep: Episode) -> None:
         if await self.check_complete(scrape_item.url):
             return
 
-        m3u8_url = _build_stream_url(boot, ep.stitched)
+        m3u8_url = _compose_stream_url(self.api.stitcher, ep.stitched)
         m3u8, info = await self.request_m3u8_playlist(m3u8_url)
         filename = self.create_custom_filename(
             ep_name(ep.season, ep.number, ep.name),
@@ -100,8 +101,10 @@ def ep_name(season: int | None, ep: int | None, name: str | None = None) -> str:
     return title
 
 
+_STITCHER: ContextVar[Stitcher] = ContextVar("_STITCHER")
+
+
 class PlutoAPI(API):
-    ENTRYPOINT: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://api.pluto.tv")
     BOOT: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://boot.pluto.tv/v4/start")
     FIREFOX: ClassVar[str] = "Mozilla/5.0 (X11; Linux x86_64; rv:151.0) Gecko/20100101 Firefox/151.0"
 
@@ -111,6 +114,10 @@ class PlutoAPI(API):
 
     def __repr__(self) -> str:
         return f"<{type(self).__name__}(app_version={self.app_version.get()}, client_id={self._client_id}, device_version={self._device_version})>"
+
+    @property
+    def stitcher(self) -> Stitcher:
+        return _STITCHER.get()
 
     @cached_method()
     async def app_version(self) -> str:
@@ -132,22 +139,29 @@ class PlutoAPI(API):
         )
         return await self.request_json(url, headers={"User-Agent": self.FIREFOX})
 
-    async def media(self, media_id: str) -> tuple[Stitcher, dict[str, Any]]:
+    async def media(self, media_id: str) -> dict[str, Any]:
         boot = await self.boot(seriesIDs=media_id)
         media = next(v for v in boot.get("VOD", []) if v["id"] == media_id)
-        return Stitcher.parse(boot), media
+        _STITCHER.set(
+            Stitcher(
+                server=self.parse_url(boot["servers"]["stitcher"]),
+                params=boot["stitcherParams"],
+                token=boot["sessionToken"],
+            )
+        )
+        return media
 
-    async def series(self, series_id: str) -> tuple[Stitcher, Series]:
-        boot, media = await self.media(series_id)
-        return boot, deserialize(Series, media)
+    async def series(self, series_id: str) -> Series:
+        media = await self.media(series_id)
+        return deserialize(Series, media)
 
 
-def _build_stream_url(boot: Stitcher, path: str) -> AbsoluteHttpURL:
+def _compose_stream_url(stitcher: Stitcher, path: str) -> AbsoluteHttpURL:
     return (
-        (boot.server / f"v2{path}")
-        .update_query(boot.params)
+        (stitcher.server / f"v2{path}")
+        .update_query(stitcher.params)
         .update_query(
-            jwt=boot.token,
+            jwt=stitcher.token,
             includeExtendedEvents="true",
             masterJWTPassthrough="true",
         )
@@ -159,14 +173,6 @@ class Stitcher:
     server: AbsoluteHttpURL
     params: str
     token: str
-
-    @staticmethod
-    def parse(boot: dict[str, Any]) -> Stitcher:
-        return Stitcher(
-            server=parse_url(boot["servers"]["stitcher"]),
-            params=boot["stitcherParams"],
-            token=boot["sessionToken"],
-        )
 
 
 class Season(TypedDict):
