af5156752b872375d1885efcadc4bb26e6fca871
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-07 18:23:30 -0500

    tests: update tests

--- tests/crawlers/test_cases/monstercat.py
+++ tests/crawlers/test_cases/monstercat.py
@@ -6,7 +6,7 @@ TEST_CASES = [
             {
                 "url": "https://www.monstercat.com/api/release/af0830b2-0318-4411-b336-e1684774f0f0/track-stream/b381e5aa-3629-497c-b451-388f28a6077b",
                 "filename": "FLUTTER.mp3",
-                "debrid_link": None,
+                "debrid_url": None,
                 "original_filename": "FLUTTER",
                 "referer": "https://www.monstercat.com/release/742779563446",
                 "album_id": "742779563446",
@@ -16,7 +16,7 @@ TEST_CASES = [
             {
                 "url": "https://www.monstercat.com/api/release/af0830b2-0318-4411-b336-e1684774f0f0/track-stream/02af32d1-200c-4eab-ae0b-9823bdf4cdfb",
                 "filename": "STEP OUT.mp3",
-                "debrid_link": None,
+                "debrid_url": None,
                 "original_filename": "STEP OUT",
                 "referer": "https://www.monstercat.com/release/742779563446",
                 "album_id": "742779563446",
