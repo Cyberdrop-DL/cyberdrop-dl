cb50c3b64a54be5130017b34526afc8dae97dbe1
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-09 19:36:59 -0500

    test: update tests

--- tests/crawlers/test_cases/daftporn.py
+++ tests/crawlers/test_cases/daftporn.py
@@ -6,7 +6,7 @@ TEST_CASES = [
             {
                 "url": "https://www.daftporn.com/movies/bar_26_05_20.mp4",
                 "filename": "Bar waitress made a new friend last night.mp4",
-                "debrid_link": None,
+                "debrid_url": None,
                 "original_filename": "Bar waitress made a new friend last night",
                 "referer": "https://www.daftporn.com/extreme-videos/Bar-waitress-made-a-new-friend-last-night.php",
                 "album_id": None,
