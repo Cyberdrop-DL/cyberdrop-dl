f1234839d47162cd6d73feea276c03afb6b8fa8a
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-19 17:13:55 -0500

    test: add test cases

---
+++ tests/crawlers/test_cases/motherless.py
@@ -0,0 +1,35 @@
+DOMAIN = "motherless"
+TEST_CASES = [
+    {
+        "url": "https://motherless.xxx/C03E162",
+        "results": [
+            {
+                "url": "re:.motherlessmedia.com/images/C03E162.jpg",
+                "filename": "C03E162 [C03E162].jpg",
+                "debrid_url": None,
+                "original_filename": "C03E162.jpg",
+                "referer": "https://motherless.xxx/C03E162",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:Loose Files (Motherless)",
+            }
+        ],
+        "count": 1,
+    },
+    {
+        "url": "https://motherless.xxx/D1C90DF",
+        "results": [
+            {
+                "url": "re:.motherlessmedia.com/videos/D1C90DF-720p.mp4",
+                "filename": "Loot [D1C90DF].mp4",
+                "debrid_url": None,
+                "original_filename": "D1C90DF-720p.mp4",
+                "referer": "https://motherless.xxx/D1C90DF",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:Loose Files (Motherless)",
+            }
+        ],
+        "count": 1,
+    },
+]
