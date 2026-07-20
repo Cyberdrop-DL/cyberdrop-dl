c7c30fc79ce02698dd6f93955a9179c684fba6c2
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-20 04:21:09 -0500

    test: update tests

--- tests/crawlers/test_cases/motherless.py
+++ tests/crawlers/test_cases/motherless.py
@@ -7,26 +7,26 @@ TEST_CASES = [
                 "url": "re:.motherlessmedia.com/images/C03E162.jpg",
                 "filename": "C03E162 [C03E162].jpg",
                 "debrid_url": None,
-                "original_filename": "C03E162.jpg",
+                "original_filename": "C03E162",
                 "referer": "https://motherless.xxx/C03E162",
                 "album_id": None,
-                "uploaded_at": None,
+                "uploaded_at": 1784409415,
                 "download_folder": "re:Loose Files (Motherless)",
             }
         ],
         "count": 1,
     },
     {
-        "url": "https://motherless.xxx/D1C90DF",
+        "url": "https://motherless.com/D1C90DF",
         "results": [
             {
                 "url": "re:.motherlessmedia.com/videos/D1C90DF-720p.mp4",
                 "filename": "Loot [D1C90DF].mp4",
                 "debrid_url": None,
-                "original_filename": "D1C90DF-720p.mp4",
+                "original_filename": "Loot",
                 "referer": "https://motherless.xxx/D1C90DF",
                 "album_id": None,
-                "uploaded_at": None,
+                "uploaded_at": 1784402707,
                 "download_folder": "re:Loose Files (Motherless)",
             }
         ],
