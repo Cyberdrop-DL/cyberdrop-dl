081319ecdf42a512533d43541c913cb927be0b98
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-21 10:35:08 -0500

    refactor: use API class

--- cyberdrop_dl/crawlers/pmvhaven.py
+++ cyberdrop_dl/crawlers/pmvhaven.py
@@ -10,77 +10,33 @@ from cyberdrop_dl import aio
 from cyberdrop_dl.crawlers.crawler import API, Crawler, SupportedPaths
 from cyberdrop_dl.mediaprops import Resolution
 from cyberdrop_dl.url_objects import AbsoluteHttpURL
-from cyberdrop_dl.utils import nuxt, unique
-from cyberdrop_dl.utils.dataclass import _DataClass, deserialize
+from cyberdrop_dl.utils import nuxt, parse_url, unique
+from cyberdrop_dl.utils.dataclass import Deserializer
 from cyberdrop_dl.utils.errors import error_handling_wrapper
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator, Generator, Iterable, Mapping
+    from collections.abc import AsyncGenerator, AsyncIterable, Iterable
     from typing import Any
 
     from cyberdrop_dl.url_objects import ScrapeItem
 
 
-PRIMARY_URL = AbsoluteHttpURL("https://pmvhaven.com")
-
-
-def with_aliases(aliases: Mapping[str, str]):
-    def _deserialize[T: _DataClass](cls: type[T], data: Mapping[str, Any], **overrides: Any) -> T:
-        for name, alias in aliases.items():
-            if name in data or name in overrides:
-                continue
-            try:
-                overrides[name] = data[alias]
-            except KeyError:
-                pass
-
-        return deserialize(cls, data, **overrides)
-
-    return _deserialize
-
-
-@dataclasses.dataclass(slots=True)
-class Video:
-    # TODO: parse and download previews and thumbnails
-    id: str
-    title: str
-    videoUrl: str
-    uploadDate: str
-    width: int
-    height: int
-
-    hlsMasterPlaylistUrl: str | None = None
-
-    @property
-    def web_url(self) -> AbsoluteHttpURL:
-        # The title does not matter, the website parses the id from the url and redirects to the correct video
-        return PRIMARY_URL / f"video/{self.title.lower()}_{self.id}"
-
-
-@dataclasses.dataclass(slots=True)
-class Playlist:
-    id: str
-    name: str
-    descrption: str
-    thumbnail: AbsoluteHttpURL
-    createdAt: str
-    updatedAt: str
-    videos: list[str]
-
-
 class PMVHavenCrawler(Crawler):
     SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
-        "Playlist": "/playlists/...",
-        "Search results": "/search/...",
+        "Playlist": "/playlists/<playlist_id>",
+        "Search results": "/search?q=<query>",
         "Users": (
-            "/profile/...",
-            "/users/...",
+            "/profile/<user>",
+            "/users/<user>",
         ),
         "Video": "/video/...",
     }
-    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://pmvhaven.com")
     DOMAIN: ClassVar[str] = "pmvhaven"
     FOLDER_DOMAIN: ClassVar[str] = "PMVHaven"
+
+    def __post_init__(self) -> None:
+        self.api: PMVAPI = PMVAPI.from_crawler(self)
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         match scrape_item.url.parts[1:]:
@@ -110,47 +66,49 @@ class PMVHavenCrawler(Crawler):
     @error_handling_wrapper
     async def playlist(self, scrape_item: ScrapeItem, playlist_id: str) -> None:
         # TODO: use playlist as album_id to skip downloads faster
-        api_url = self.PRIMARY_URL / "api/playlists" / playlist_id
-        data, pages = await aio.peek_first(self._api_pager(api_url))
-        title = self.create_title(f"{data['name']} [playlist]")
+        playlist, video_pages = await self.api.playlist(playlist_id)
+        title = self.create_title(f"{playlist.name} [playlist]")
         scrape_item.setup_as_album(title)
+        await self._playlist(scrape_item, playlist)
+        await self._iter_videos(scrape_item, video_pages)
 
-        async for data in pages:
-            for video in _parse_videos(data["videoDetails"]):
+    async def _playlist(self, scrape_item: ScrapeItem, playlist: Playlist) -> None:
+        self.create_eager_task(self.write_metadata(scrape_item, playlist.id, playlist))
+        with self.catch_errors(playlist.thumbnail):
+            _, ext = self.get_filename_and_ext(playlist.thumbnail.name)
+            filename = self.create_custom_filename(playlist.name, ext)
+            await self.handle_file(
+                playlist.thumbnail,
+                scrape_item,
+                playlist.thumbnail.name,
+                ext,
+                frag="thumbnail",
+                custom_filename=filename,
+            )
+
+    async def _iter_videos(self, scrape_item: ScrapeItem, video_pages: AsyncIterable[Iterable[Video]]) -> None:
+        async for videos in video_pages:
+            for video in videos:
                 await self._video(scrape_item.copy(), video)
                 scrape_item.add_children()
-
-    async def _api_pager(self, api_url: AbsoluteHttpURL, init_page: int = 1) -> AsyncGenerator[Any]:
-        for page in itertools.count(init_page):
-            resp: dict[str, Any] = await self.request_json(api_url.update_query(limit=100, page=page))
-            yield resp["data"]
-            if not resp["pagination"]["hasMore"]:
-                break
 
     @error_handling_wrapper
     async def search(self, scrape_item: ScrapeItem, query: str) -> None:
         title = self.create_title(f"{query} [search]")
         scrape_item.setup_as_profile(title)
-        api_url = (self.PRIMARY_URL / "api/videos/search").with_query(q=query)
-        data: list[dict[str, Any]]
-        async for data in self._api_pager(api_url):
-            for video in _parse_videos(data):
-                await self._video(scrape_item.copy(), video)
-                scrape_item.add_children()
+        await self._iter_videos(scrape_item, self.api.search(query))
 
     @error_handling_wrapper
     async def video(self, scrape_item: ScrapeItem, video_id: str) -> None:
         if await self.check_complete_from_referer(scrape_item.url):
             return
 
-        api_url = self.PRIMARY_URL / "api/videos" / video_id
-        data = (await self.request_json(api_url))["data"]
-        video = Video.from_dict(data)
+        video = await self.api.video(video_id)
         await self._video(scrape_item, video)
 
     @error_handling_wrapper
     async def _video(self, scrape_item: ScrapeItem, video: Video) -> None:
-        scrape_item.url = video.web_url
+        scrape_item.url = self.PRIMARY_URL / video.href
         scrape_item.uploaded_at = self.parse_iso_date(video.uploadDate)
         link = self.parse_url(video.videoUrl)
         filename, ext = self.get_filename_and_ext(link.name, assume_ext=".mp4")
@@ -163,28 +121,62 @@ class PMVHavenCrawler(Crawler):
         await self.handle_file(link, scrape_item, filename, ext, custom_filename=custom_filename, metadata=video)
 
 
+_deserialize = Deserializer(aliases={"id": "_id"}, converters={"thumbnail": parse_url})
+
+
+@dataclasses.dataclass(slots=True)
+class Video:
+    # TODO: parse and download previews and thumbnails
+    id: str
+    title: str
+    videoUrl: str
+    uploadDate: str
+    width: int
+    height: int
+
+    hlsMasterPlaylistUrl: str | None = None
+
+    @property
+    def href(self) -> str:
+        # The title does not matter, the website parses the id from the url and redirects to the correct video
+        return f"video/{self.title.lower()}_{self.id}"
+
+    parse = classmethod(_deserialize)
+
+
+@dataclasses.dataclass(slots=True)
+class Playlist:
+    id: str
+    name: str
+    descrption: str
+    thumbnail: AbsoluteHttpURL
+    createdAt: str
+    updatedAt: str
+    videos: list[str]
+
+
 class PMVAPI(API):
     async def video(self, video_id: str) -> Video:
         api_url = self.PRIMARY_URL / "api/videos" / video_id
         data = (await self.request_json(api_url))["data"]
-        return deserialize(Video, data, id=data["_id"])
+        return Video.parse(data)
 
-    async def search(self, query: str) -> AsyncGenerator[Generator[Video]]:
+    async def search(self, query: str) -> AsyncGenerator[map[Video]]:
         api_url = (self.PRIMARY_URL / "api/videos/search").with_query(q=query)
         data: list[dict[str, Any]]
         async for data in self.pager(api_url):
             yield _parse_videos(data)
 
-    async def playlist(self, playlist_id: str) -> tuple[Playlist, AsyncGenerator[Generator[Video]]]:
+    async def playlist(self, playlist_id: str) -> tuple[Playlist, AsyncGenerator[map[Video]]]:
         api_url = self.PRIMARY_URL / "api/playlists" / playlist_id
         data, pages = await aio.peek_first(self.pager(api_url))
-        playlist = deserialize(Playlist, data, id=data["_id"])
+        playlist = _deserialize(Playlist, data)
 
-        async def videos():
+        async def video_pages():
             async for data in pages:
                 yield _parse_videos(data["videoDetails"])
 
-        return playlist, videos()
+        return playlist, video_pages()
 
     async def pager(self, api_url: AbsoluteHttpURL, init_page: int = 1) -> AsyncGenerator[Any]:
         for page in itertools.count(init_page):
@@ -195,7 +187,4 @@ class PMVAPI(API):
 
 
 def _parse_videos(videos: Iterable[dict[str, Any]]) -> map[Video]:
-    videos = unique(videos, operator.itemgetter("_id"))
-    return map(
-        _deserialize,
-    )
+    return map(Video.parse, unique(videos, operator.itemgetter("_id")))
