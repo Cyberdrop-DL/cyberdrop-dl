f3a87787282847b09220a8deb1504828ab46e6c4
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-22 04:30:57 -0500

    test: update test cases

--- tests/crawlers/test_cases/twitter.py
+++ tests/crawlers/test_cases/twitter.py
@@ -12,7 +12,34 @@ TEST_CASES = [
                 "album_id": None,
                 "uploaded_at": 1769115874,
                 "download_folder": "re:@matthewisabel (Twitter)/2026-01-22 - 2014444098690613718",
+            },
+            {
+                "url": "https://pbs.twimg.com/media/G_S962_WAAAJqHh.jpg?format=jpg&name=orig",
+                "filename": "G_S962_WAAAJqHh.jpg",
+                "debrid_url": None,
+                "original_filename": "G_S962_WAAAJqHh.jpg",
+                "referer": "https://pbs.twimg.com/media/G_S962_WAAAJqHh.jpg?name=orig",
+                "album_id": None,
+                "uploaded_at": 1769115874,
+                "download_folder": "re:@matthewisabel (Twitter)/2026-01-22 - 2014444098690613718",
+            },
+        ],
+        "count": 2,
+    },
+    {
+        "url": "https://x.com/SpaceX/status/2079677787330289882",
+        "results": [
+            {
+                "url": "https://video.twimg.com/amplify_video/2079677647064317952/vid/avc1/3840x2160/85gQfk4JevBgdoUR.mp4?tag=29",
+                "filename": "85gQfk4JevBgdoUR.mp4",
+                "debrid_url": None,
+                "original_filename": "85gQfk4JevBgdoUR.mp4",
+                "referer": "https://x.com/SpaceX/status/2079677787330289882",
+                "album_id": None,
+                "uploaded_at": 1784668797,
+                "download_folder": "re:@SpaceX (Twitter)/2026-07-21 - 2079677787330289882",
             }
         ],
-    }
+        "count": 1,
+    },
 ]
