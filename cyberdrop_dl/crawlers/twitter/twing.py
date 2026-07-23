edd142b25110910281fd5a0471e2efe4b30e4a80
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-23 02:15:09 -0500

    refactor: update twing

--- cyberdrop_dl/crawlers/twitter/twing.py
+++ cyberdrop_dl/crawlers/twitter/twing.py
@@ -8,48 +8,40 @@ from cyberdrop_dl.url_objects import AbsoluteHttpURL, MediaItem
 from cyberdrop_dl.utils.errors import error_handling_wrapper
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
-
     from cyberdrop_dl.url_objects import ScrapeItem
     from cyberdrop_dl.utils import m3u8
 
 
-_CDN_HOST = "pbs.twimg.com"
-
-
 class TwimgCrawler(Crawler):
-    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Photo": "/..."}
+    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
+        "Photo": "/media/<media_id>...",
+        "Video": "/amplify_video/<media_id>...",
+    }
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://twimg.com/")
     DOMAIN: ClassVar[str] = "twimg"
     FOLDER_DOMAIN: ClassVar[str] = "TwitterImages"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
-        if "video" in scrape_item.url.host and scrape_item.url.suffix != ".m3u8":
+        if "amplify_video_thumb" in scrape_item.url.parts or scrape_item.url.suffix == ".m3u8":
+            raise ValueError
+        if "video" in scrape_item.url.host:
             return await self.direct_file(scrape_item)
         await self.photo(scrape_item)
 
     @error_handling_wrapper
     async def photo(self, scrape_item: ScrapeItem, url: AbsoluteHttpURL | None = None) -> None:
-        # https://developer.x.com/en/docs/x-api/v1/data-dictionary/object-model/entities#photo_format
-        link = url or scrape_item.url
-        if "emoji" in link.parts:
+        # https://docs.x.com/x-api/enterprise-gnip-2.0/fundamentals/data-dictionary#photo-media-url-formatting
+        src = url or scrape_item.url
+        if "emoji" in src.parts:
             return
 
-        # name could be "orig", "4096x4096", "large", "medium", or "small"
-        # `orig`` is original quality but it's not always available, same as "4096x4096"
-        # "large", "medium", or "small" are always available
-
-        link = next(_make_download_urls(link.with_host(_CDN_HOST)))
-        filename = Path(link.name).with_suffix(".jpg").as_posix()
-        filename, ext = self.get_filename_and_ext(filename)
-        await self.handle_file(link, scrape_item, filename, ext)
+        src = src.with_host("pbs.twimg.com").with_query(format="jpg", name=self.config.crawlers.twitter.image_size)
+        name = Path(src.name).with_suffix(".jpg").as_posix()
+        filename, ext = self.get_filename_and_ext(name)
+        await self.handle_file(src, scrape_item, name, ext, custom_filename=filename)
 
     async def handle_media_item(self, media_item: MediaItem, m3u8: m3u8.Rendition | None = None) -> None:
         if media_item.referer == media_item.url and media_item.parents:
             media_item.referer = media_item.parents[0]
+            media_item.headers["Referer"] = str(media_item.referer)
         await super().handle_media_item(media_item, m3u8)
-
-
-def _make_download_urls(base_url: AbsoluteHttpURL) -> Generator[AbsoluteHttpURL]:
-    for name in ("orig", "4096x4096", "large"):
-        yield base_url.with_query(format="jpg", name=name)
