7218910c501a1c32e1a8518fcfec8d5287702165
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-26 07:14:10 -0500

    test: fix windows default

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -157,7 +157,7 @@ class TestTextEditor:
     def test_win_default(self, tmp_cwd: Path) -> None:
         cmd = text_editor._find_win_editor()
         assert cmd == text_editor._editor_cmd()
-        assert cmd == ("notepad.exe",)
+        assert cmd == ("C:\\Windows\\system32\\notepad.exe",)
         np = tmp_cwd / "notepad++.exe"
         np.write_text("test")
         assert text_editor._find_win_editor() == (str(np), "-multiInst", "-noPlugin", "-notabbar", "-nosession")
