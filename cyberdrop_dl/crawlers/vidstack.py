665108b9c895dadc2146f2c0842b85b4804f8320
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-08-07 08:26:04 -0500

    feat: add vidstack support

---
+++ cyberdrop_dl/crawlers/vidstack.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+
+import dataclasses
+import json
+import logging
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, ClassVar, Unpack
+
+from cyberdrop_dl.clients.http import HTTPConfig
+from cyberdrop_dl.crawlers.crawler import API, Crawler, DownloadConfig, SupportedDomains, SupportedPaths, URLConfig
+from cyberdrop_dl.exceptions import ScrapeError
+from cyberdrop_dl.url_objects import AbsoluteHttpURL
+from cyberdrop_dl.utils.crypto import aes_cbc_decrypt
+from cyberdrop_dl.utils.errors import error_handling_wrapper
+
+if TYPE_CHECKING:
+    from curl_cffi.requests.session import HttpMethod
+
+    from cyberdrop_dl.clients.request import RequestParams
+    from cyberdrop_dl.url_objects import ScrapeItem
+
+logger = logging.getLogger(__name__)
+
+
+@Crawler.db_path_builder("path_qs_frag")
+@URLConfig(allow_empty_path=True, ignore_fragment=False, trim=False)
+@HTTPConfig(headers={"Origin": "https://videosh.upns.live", "Referer": "https://videosh.upns.live/"})
+@DownloadConfig(slots=2)
+class VidStackCrawler(Crawler):
+    SUPPORTED_DOMAINS: ClassVar[SupportedDomains] = ("videosh.upns.live",)
+    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Video": "/#<video_id>"}
+    DOMAIN: ClassVar[str] = "vidstack"
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://vidstack.io")
+
+    def __post_init__(self) -> None:
+        self.api: VidStackAPI = VidStackAPI.from_crawler(self)
+
+    async def fetch(self, scrape_item: ScrapeItem) -> None:
+        video_id = scrape_item.url.fragment.partition("/")[0].partition("&")[0]
+        if not video_id:
+            raise ValueError
+
+        await self.video(scrape_item, video_id)
+
+    @error_handling_wrapper
+    async def video(self, scrape_item: ScrapeItem, video_id: str) -> None:
+        if await self.check_complete(scrape_item.url):
+            return
+
+        video = await self.api.video(video_id, scrape_item.referer)
+        m3u8, info = await self.request_m3u8_playlist(video.src)
+        custom_filename = self.create_custom_filename(
+            video.title,
+            ext := ".mp4",
+            file_id=video_id,
+            resolution=info.resolution,
+        )
+
+        await self.handle_file(
+            scrape_item.url,
+            scrape_item,
+            video.title,
+            ext,
+            m3u8=m3u8,
+            custom_filename=custom_filename,
+        )
+
+        _, ext = self.get_filename_and_ext(video.thumb.name)
+        thumb_name = f"{Path(custom_filename).stem}_thumb{ext}"
+        filename, _ = self.get_filename_and_ext(thumb_name)
+        await self.handle_file(
+            scrape_item.url,
+            scrape_item,
+            thumb_name,
+            ext,
+            custom_filename=filename,
+            debrid_link=video.thumb,
+            frag="thumbnail",
+        )
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class Video:
+    id: str
+    title: str
+    thumb: AbsoluteHttpURL
+    src: AbsoluteHttpURL
+
+
+class VidStackAPI(API):
+    KEY: ClassVar[bytes] = b"kiemtienmua911ca"
+    IVS: ClassVar[tuple[bytes, ...]] = (b"1234567890oiuytr", b"0123456789abcdef")
+
+    async def video(self, video_id: str, referer: AbsoluteHttpURL | None = None) -> Video:
+        url = (self.origin / "api/v1/video").with_query(id=video_id)
+        info = await self.request_aes_json(url, referer=referer)
+        return Video(
+            video_id,
+            info["title"],
+            thumb=self.parse_url(info["poster"]),
+            src=self.parse_url(info["source"]),
+        )
+
+    async def request_aes(
+        self,
+        url: AbsoluteHttpURL,
+        method: HttpMethod = "GET",
+        referer: AbsoluteHttpURL | None = None,
+        **kwargs: Unpack[RequestParams],
+    ) -> str:
+        if referer:
+            url = url.update_query(r=str(referer))
+            kwargs.setdefault("headers", {})["Referer"] = str(referer)
+
+        text = await self.request_text(url, method, **kwargs)
+        content = bytes.fromhex(text.strip())
+
+        for iv in self.IVS:
+            try:
+                return aes_cbc_decrypt(content, self.KEY, iv).decode("utf8")
+            except Exception:
+                logger.exception("")
+                continue
+
+        raise ScrapeError(422, "Unable to decode encrypted response")
+
+    async def request_aes_json(
+        self,
+        url: AbsoluteHttpURL,
+        method: HttpMethod = "GET",
+        referer: AbsoluteHttpURL | None = None,
+        **kwargs: Unpack[RequestParams],
+    ) -> dict[str, Any]:
+        content = (await self.request_aes(url, method, referer, **kwargs)).strip()
+        try:
+            return json.loads(content)
+        except ValueError:
+            return json.loads(content[: content.rindex("}") + 1])
