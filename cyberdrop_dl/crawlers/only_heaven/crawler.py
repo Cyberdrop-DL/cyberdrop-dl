aa5bd9dffb2b5f5f409a1a7425e6779f307e46fb
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-08-09 23:34:21 -0500

    feat: add onlyhaven crawler

---
+++ cyberdrop_dl/crawlers/only_heaven/crawler.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+import dataclasses
+from collections.abc import Generator
+from typing import TYPE_CHECKING, ClassVar, override
+
+from cyberdrop_dl.crawlers.kemono.api import KemonoAPI
+from cyberdrop_dl.crawlers.kemono.kemono import KemonoBaseCrawler
+from cyberdrop_dl.crawlers.only_heaven.models import File, UserPostModel
+from cyberdrop_dl.exceptions import NoExtensionError
+from cyberdrop_dl.url_objects import AbsoluteHttpURL
+from cyberdrop_dl.utils import unique
+from cyberdrop_dl.utils.errors import error_handling_wrapper
+
+if TYPE_CHECKING:
+    import logging
+    from collections.abc import Generator
+
+    from cyberdrop_dl.config.crawlers import KemonoConfig
+    from cyberdrop_dl.url_objects import ScrapeItem
+
+
+class OnlyHavenAPI(KemonoAPI[UserPostModel]):
+    ENTRYPOINT: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://cum.st/api/v1")
+
+
+class OnlyHavenCrawler(KemonoBaseCrawler):
+    __kemono_api__: ClassVar[type[KemonoAPI]] = OnlyHavenAPI  # pyright: ignore[reportAssignmentType]
+    __kemono_cdn__: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://e1.cum.st/media")
+
+    async def fetch(self, scrape_item: ScrapeItem) -> None:
+        match scrape_item.url.parts[1:]:
+            case [service, "user", creator_id, "post", post_id]:
+                return await self.post(scrape_item, service, creator_id, post_id)
+            case [service, "user", creator_id]:
+                return await self.creator(scrape_item, service, creator_id)
+            case ["posts"] if search_query := scrape_item.url.query.get("q"):
+                return await self.search(scrape_item, search_query)
+            case _:
+                raise ValueError
+
+    @error_handling_wrapper
+    async def _direct_file(self, scrape_item: ScrapeItem, url: AbsoluteHttpURL | None = None) -> None:
+        link = url or scrape_item.url
+        checksum = link.parent.name
+        if await self.check_complete_by_hash(link, "sha256", checksum):
+            return
+
+        name = link.query.get("f") or link.name
+        try:
+            filename, ext = self.get_filename_and_ext(name)
+        except NoExtensionError:
+            # Some patreon URLs have another URL as the filename:
+            # ex: https://kemono.su/data/7a...27ad7e40bd.jpg?f=https://www.patreon.com/media-u/Z0F..00672794_
+            filename, ext = self.get_filename_and_ext(link.name)
+
+        await self.handle_file(link, scrape_item, name, ext, custom_filename=filename)
+
+    @override
+    def _post(self, scrape_item: ScrapeItem, post: UserPostModel) -> None:  # pyright: ignore[reportIncompatibleMethodOverride]
+        scrape_item.uploaded_at = post.timestamp
+        self.create_eager_task(self.write_metadata(scrape_item, f"post_{post.id}", post))
+        files = FileFilterer(post, self.__kemono_config__, self.log)
+        try:
+            self._extract_post_files(scrape_item, files)
+        finally:
+            self.tui.files.stats.skipped += files.skipped
+        self._extract_urls_from_post_content(scrape_item, post)
+
+    @override
+    def _extract_post_files(self, scrape_item: ScrapeItem, post_files: FileFilterer) -> None:  # pyright: ignore[reportIncompatibleMethodOverride]
+        for url in unique(map(self._compose_file_url, post_files)):
+            self.create_eager_task(self._direct_file(scrape_item, url))
+            scrape_item.add_children()
+
+        if embed := post_files.embed():
+            embed_url = self.parse_url(embed.url)
+            self.handle_external_links(scrape_item.create_child(embed_url))
+            scrape_item.add_children()
+
+    @override
+    def _compose_file_url(self, file: File) -> AbsoluteHttpURL:  # pyright: ignore[reportIncompatibleMethodOverride]
+        url = self.__kemono_cdn__ / "media" / file.storageKey / max(file.variants).name
+        return url.update_query(f=file.originalFilename or url.name)
+
+
+@dataclasses.dataclass(slots=True)
+class FileFilterer:
+    post: UserPostModel
+    config: KemonoConfig
+    log: logging.LoggerAdapter[logging.Logger] | logging.Logger
+    skipped: int = dataclasses.field(init=False, default=0)
+
+    def _files(self) -> Generator[tuple[File, str, bool]]:
+        for file in self.post.attachments:
+            yield file, "attachment", self.config.attachments
+
+    def __iter__(self) -> Generator[File]:
+        for file, kind, should_download in self._files():
+            file_name = file.originalFilename
+            if not should_download:
+                self._report_skip_by_config(file_name, kind)
+            else:
+                yield file
+
+    def _report_skip_by_config(self, name: str, kind: str) -> None:
+        self.log.info("Skipping file '%s' in post #%s by config options [%s]", name, self.post.id, kind)
+        self.skipped += 1
+
+    def embed(self) -> None:
+        return None
---
+++ cyberdrop_dl/crawlers/only_heaven/models.py
@@ -0,0 +1,59 @@
+import dataclasses
+from typing import Any, override
+
+from pydantic import Field
+
+from cyberdrop_dl.crawlers.kemono.models import PostProtocol, User
+from cyberdrop_dl.models import DeferredModel
+from cyberdrop_dl.models.types import AwareDatetime
+
+
+@dataclasses.dataclass(slots=True, frozen=True, order=True)
+class Variant:
+    bytes: int
+    name: str
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class File:
+    id: str
+    sha256: str
+    kind: str
+    mimeType: str  # noqa: N815
+    width: int
+    height: int
+    storageKey: str  # noqa: N815
+    variants: tuple[Variant, ...]
+    originalFilename: str  # noqa: N815
+
+
+class PostModel(PostProtocol[File], DeferredModel, extra="ignore"):
+    id: str
+    content: str | None = Field(validation_alias="caption", default=None)
+    attachments: tuple[File, ...] = ()
+    published: AwareDatetime | None = None
+    added: AwareDatetime | None = None
+    timestamp: int | None = None
+    links: tuple[Any, ...] = ()
+    tags: tuple[str, ...] = ()
+    preview_state: str | None = None
+    has_full: bool = True
+
+    @override
+    def model_post_init(self, *_: object) -> None:
+        if date := self.published or self.added:
+            self.timestamp = int(date.timestamp())
+
+
+class UserPostModel(PostModel):
+    service: str
+    user_id: str
+    title: str | None = None
+
+    @property
+    def user(self) -> User:
+        return User(self.service, self.user_id)
+
+    @property
+    def web_path_qs(self) -> str:
+        return f"{self.service}/user/{self.user_id}/post/{self.id}"
