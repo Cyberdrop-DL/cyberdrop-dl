70b8f831d146e8974cd8539c1dbc9f468b143219
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-08-11 18:38:18 -0500

    feat: add series support

--- cyberdrop_dl/crawlers/pluto_tv.py
+++ cyberdrop_dl/crawlers/pluto_tv.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import dataclasses
+import itertools
 import uuid
 from contextvars import ContextVar
 from typing import TYPE_CHECKING, Any, ClassVar, override
 
 import yarl
+from typing_extensions import AsyncGenerator
 
 from cyberdrop_dl.cache import cached_method
 from cyberdrop_dl.crawlers.crawler import API, Crawler, SupportedPaths, compose_ep_name
@@ -17,12 +19,15 @@ from cyberdrop_dl.utils.errors import error_handling_wrapper
 session_token: ContextVar[str] = ContextVar("session_token")
 
 if TYPE_CHECKING:
+    from collections.abc import Generator
+
     from cyberdrop_dl.utils.m3u8 import Rendition
 
 
 class PlutoCrawler(Crawler):
     SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
-        "Episode": (".../shows/<series_id>/episode/<episode_id>",),
+        "Episode": (".../shows/<show_id>/episode/<episode_id>",),
+        "Show": (".../shows/<show_slug>",),
     }
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://pluto.tv")
     DOMAIN: ClassVar[str] = "pluto.tv"
@@ -39,13 +44,24 @@ class PlutoCrawler(Crawler):
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         match scrape_item.url.parts[1:]:
-            case [*_, "shows", series_id, "episode", episode_id]:
-                await self.episode(scrape_item, series_id, episode_id)
-            case [*_, "shows", series_id, "season", _season]:
-                raise ValueError
+            case [*_, "shows", show_id, "episode", episode_id]:
+                await self.episode(scrape_item, show_id, episode_id)
+            case [*_, "shows", show_id]:
+                await self.show(scrape_item, show_id)
+            case [*_, "shows", show_id, "season", _season]:
+                await self.show(scrape_item, show_id)
 
             case _:
                 raise ValueError
+
+    @error_handling_wrapper
+    async def show(self, scrape_item: ScrapeItem, show_slug: str) -> None:
+        soup = await self.request_soup(scrape_item.url)
+        data = next_js.data(soup)
+        ep = data["props"]["pageProps"]["dehydratedState"]["queries"]
+        episode = _deserialize(Episode, ep, id=show_slug)
+        scrape_item.setup_as_album(self.create_title(ep["seriesTitle"], show_slug), album_id=show_slug)
+        await self._episode(scrape_item, episode)
 
     @error_handling_wrapper
     async def episode(self, scrape_item: ScrapeItem, series_id: str, episode_id: str) -> None:
@@ -87,9 +103,9 @@ class PlutoCrawler(Crawler):
 
 
 class PlutoAPI(API):
-    BOOT: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://boot.pluto.tv/v4/start")
     FIREFOX: ClassVar[str] = "Mozilla/5.0 (X11; Linux x86_64; rv:151.0) Gecko/20100101 Firefox/151.0"
     GRAPHQL_ENDPOINT: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://pluto.tv/api/tn/app-shell/graphql/")
+    SERIES: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://service-vod.clusters.pluto.tv/v4/vod/series/")
     M3U8: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL(
         "https://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/v2/stitch/hls/episode"
     )
@@ -106,6 +122,7 @@ class PlutoAPI(API):
         soup = await self.request_soup(self.PRIMARY_URL)
         return css.select(soup, "meta[name=appVersion], meta[name=app_version]", "content")
 
+    @cached_method(ttl=1800)
     async def start(self) -> Session:
         resp = await self.request_gql(
             "PtvStart",
@@ -140,7 +157,6 @@ class PlutoAPI(API):
     async def stream(self, episode_id: str) -> AbsoluteHttpURL:
         session = await self.start()
         url = self.M3U8 / episode_id / "master.m3u8"
-        sid = str(uuid.uuid1())
         return url.with_query(
             {
                 "advertisingId": "",
@@ -154,13 +170,20 @@ class PlutoAPI(API):
                 "deviceType": "web",
                 "deviceVersion": "151.0",
                 "serverSideAds": "false",
-                "sessionID": sid,
-                "sid": sid,
+                "sessionID": session.id,
+                "sid": session.id,
                 "userId": "",
                 "jwt": session.jwt,
                 "includeExtendedEvents": "true",
             }
         )
+
+    async def series_seasons(self, series_id: str) -> AsyncGenerator[Generator[Episode]]:
+        session = await self.start()
+        rul = (self.SERIES / series_id / "seasons").with_query(offset=0)
+        for page in itertools.count(0):
+            resp = await self.request_json(rul.update_query(page=page), headers={"Autorization": session.jwt})
+            yield (_deserialize(Episode, ep) for ep in resp["seasons"]["episodes"])
 
 
 @dataclasses.dataclass(slots=True, frozen=True)
