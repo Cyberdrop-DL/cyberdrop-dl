aefb70eb0008e87360068fefde4512b3eca4e5ef
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-08-13 20:23:05 -0500

    test: add test case

---
+++ tests/crawlers/test_cases/octave_music.py
@@ -0,0 +1,19 @@
+DOMAIN = "music.octavestreaming"
+TEST_CASES = [
+    {
+        "url": "https://music.octavestreaming.com/album/983236081?t=4022917771",
+        "results": [
+            {
+                "url": "https://music.octavestreaming.com/album/983236081?t=4022917771",
+                "filename": "Drake - Janice STFU.mp3",
+                "debrid_url": "re:https://api.octavestreaming.com/audio/320?track=4022917771&k=",
+                "original_filename": "Drake - Janice STFU.mp3",
+                "referer": "https://music.octavestreaming.com/album/983236081?t=4022917771",
+                "album_id": None,
+                "uploaded_at": 1778821200,
+                "download_folder": "re:Loose Files (OctaveMusic)",
+            }
+        ],
+        "count": 1,
+    }
+]
