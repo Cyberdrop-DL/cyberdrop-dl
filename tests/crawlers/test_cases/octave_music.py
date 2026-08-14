ca747fcddbd0aad872e82e644575300acfbdfe96
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-08-14 16:40:47 -0500

    tests: add album test case

--- tests/crawlers/test_cases/octave_music.py
+++ tests/crawlers/test_cases/octave_music.py
@@ -15,5 +15,22 @@ TEST_CASES = [
             }
         ],
         "count": 1,
-    }
+    },
+    {
+        "url": "https://music.octavestreaming.com/album/983217461",
+        "results": [
+            {
+                "url": "https://cdn-images.dzcdn.net/images/cover/a3c0b45062f3987b928b13cd6122f443/1000x1000-000000-80-0-0.jpg",
+                "filename": "cover [983217461].jpg",
+                "debrid_url": None,
+                "original_filename": "1000x1000-000000-80-0-0.jpg",
+                "referer": "https://music.octavestreaming.com/album/983217461",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:HABIBTI (OctaveMusic)",
+                "thumbnail": None,
+            },
+        ],
+        "count": 12,
+    },
 ]
