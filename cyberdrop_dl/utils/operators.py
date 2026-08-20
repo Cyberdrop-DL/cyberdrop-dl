2fe5ba4ae515e1b478f67f967c20e640de819172
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-08-20 05:37:17 -0500

    perf: split keys once

--- cyberdrop_dl/utils/operators.py
+++ cyberdrop_dl/utils/operators.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, final
 
 
+@final
 class nested_itemgetter:  # noqa: N801
     def __init__(self, key: str, *keys: str) -> None:
         self.keys: tuple[str, ...] = key, *keys
@@ -22,21 +23,21 @@ class nested_itemgetter:  # noqa: N801
         return f"{type(self).__name__}({', '.join(map(repr, self.keys))})"
 
 
+@final
 class nested_itemsetter:  # noqa: N801
     def __init__(self, key: str, *keys: str) -> None:
-        self.keys: tuple[str, ...] = key, *keys
+        self.keys = key, *keys
+        self._lead_keys = self.keys[:-1]
+        self._tail = self.keys[-1]
 
     __repr__ = nested_itemgetter.__repr__
 
     def __call__(self, obj: Any, value: Any) -> Any:
-        path: list[str] = []
-        *keys, last = self.keys
-        for key in keys:
-            path.append(key)
+        for key in self._lead_keys:
             try:
                 obj = obj[key]
             except KeyError:
                 obj[key] = {}
                 obj = obj[key]
 
-        obj[last] = value
+        obj[self._tail] = value
